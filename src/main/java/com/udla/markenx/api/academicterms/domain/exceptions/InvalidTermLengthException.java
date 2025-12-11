99313025a55d5789108c2988bf471abf98ba661c
ChrisJMora2003 <christian.jacome.mora@udla.edu.ec>
2025-12-11 17:11:23 -0500

    feat: Use domain exception for scenario: invalid term length

---
+++ src/main/java/com/udla/markenx/api/academicterms/domain/exceptions/InvalidTermLengthException.java
@@ -0,0 +1,24 @@
+package com.udla.markenx.api.academicterms.domain.exceptions;
+
+import lombok.Getter;
+
+@Getter
+public class InvalidTermLengthException extends AcademicTermException {
+
+    private final long actualMonths;
+    private final int minMonths;
+    private final int maxMonths;
+
+    public InvalidTermLengthException(long actualMonths, int minMonths, int maxMonths) {
+        super(String.format(
+                "La duración del período académico (%d meses) es inválida. " +
+                        "Debe tener entre %d y %d meses de duración",
+                actualMonths,
+                minMonths,
+                maxMonths
+        ));
+        this.actualMonths = actualMonths;
+        this.minMonths = minMonths;
+        this.maxMonths = maxMonths;
+    }
+}
