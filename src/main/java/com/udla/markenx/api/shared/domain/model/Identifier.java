4e36eb40ad049063bbdc3162bc9ed3d07e2bcc81
ChrisJMora2003 <christian.jacome.mora@udla.edu.ec>
2025-12-11 14:56:56 -0500

    feat: Add general identifier to use in aggregates of all domains

---
+++ src/main/java/com/udla/markenx/api/shared/domain/model/Identifier.java
@@ -0,0 +1,35 @@
+package com.udla.markenx.api.shared.domain.model;
+
+import java.util.Objects;
+
+public abstract class Identifier {
+    protected final String value;
+
+    protected Identifier(String value) {
+        if (value == null || value.isBlank()) {
+            throw new IllegalArgumentException("Identifier cannot be null or empty");
+        }
+        this.value = value;
+    }
+
+    public String value() {
+        return value;
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Identifier that)) return false;
+        return Objects.equals(value, that.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
+}
