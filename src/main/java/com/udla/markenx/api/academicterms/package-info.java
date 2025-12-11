bfc927e7c3a2afc601a24aa27558c40e13a94382
ChrisJMora2003 <christian.jacome.mora@udla.edu.ec>
2025-12-11 15:11:38 -0500

    feat: Add shared as a dependency of manage terms domain

--- src/main/java/com/udla/markenx/api/academicterms/package-info.java
+++ src/main/java/com/udla/markenx/api/academicterms/package-info.java
@@ -1,4 +1,4 @@
-@ApplicationModule(displayName = "Term Management")
+@ApplicationModule(displayName = "Term Management", allowedDependencies = {"shared"})
 package com.udla.markenx.api.academicterms;
 
 import org.springframework.modulith.ApplicationModule;
