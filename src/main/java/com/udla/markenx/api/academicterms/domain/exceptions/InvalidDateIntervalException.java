284e4ca2d2388f83e56557db438e4b8b276c9450
ChrisJMora2003 <christian.jacome.mora@udla.edu.ec>
2025-12-11 14:25:03 -0500

    feat: Add exception for the scenario: invalid date interval (end date is before start date)

---
+++ src/main/java/com/udla/markenx/api/academicterms/domain/exceptions/InvalidDateIntervalException.java
@@ -0,0 +1,17 @@
+package com.udla.markenx.api.academicterms.domain.exceptions;
+
+import java.time.LocalDate;
+
+public class InvalidDateIntervalException extends DateIntervalException {
+
+    public InvalidDateIntervalException(LocalDate startDate, LocalDate endDate) {
+        super(String.format(
+                "El intervalo de fechas es inválido: la fecha de fin (%s) no puede ser anterior a la fecha de inicio (%s)",
+                endDate, startDate
+        ));
+    }
+
+    public InvalidDateIntervalException(String message) {
+        super(message);
+    }
+}
