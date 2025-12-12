c0725a9fdd437003f5a6b26b810acbcac89c1af3
ChrisJMora2003 <christian.jacome.mora@udla.edu.ec>
2025-12-12 15:02:22 -0500

    feat: Add academic term repository

---
+++ src/main/java/com/udla/markenx/api/academicterms/infrastructure/persistance/jpa/JpaAcademicTermRepository.java
@@ -0,0 +1,29 @@
+package com.udla.markenx.api.academicterms.infrastructure.persistance.jpa;
+
+import com.udla.markenx.api.academicterms.domain.models.aggregates.AcademicTerm;
+import com.udla.markenx.api.academicterms.domain.ports.outgoing.AcademicTermRepository;
+import com.udla.markenx.api.academicterms.infrastructure.mappers.AcademicTermJpaMapper;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class JpaAcademicTermRepository implements AcademicTermRepository {
+
+    private final SpringDataAcademicTermRepository springRepo;
+    private final AcademicTermJpaMapper mapper;
+
+    public JpaAcademicTermRepository(SpringDataAcademicTermRepository springRepo,
+                                     AcademicTermJpaMapper mapper) {
+        this.springRepo = springRepo;
+        this.mapper = mapper;
+    }
+
+    @Override
+    public Page<@NotNull AcademicTerm> findAllPaginated(Pageable pageable) {
+        return springRepo.findAll(pageable).map(mapper::toDomain);
+    }
+}
