2306a977de27bf08b872bb409ddc6cd0b7e51bc2
ChrisJMora2003 <christian.jacome.mora@udla.edu.ec>
2025-12-12 15:44:53 -0500

    refactor: Delete unnecessary import in JpaAcademicTermRepository.java

--- src/main/java/com/udla/markenx/api/academicterms/infrastructure/persistance/jpa/JpaAcademicTermRepository.java
+++ src/main/java/com/udla/markenx/api/academicterms/infrastructure/persistance/jpa/JpaAcademicTermRepository.java
@@ -8,8 +8,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public class JpaAcademicTermRepository implements AcademicTermRepository {
 
