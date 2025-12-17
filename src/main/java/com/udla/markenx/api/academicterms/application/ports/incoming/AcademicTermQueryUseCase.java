a9d3f2ee5c7cfbcb9f664bdfc2b66e07f8ff3629
Christian Jacome <christian.jacome.mora@udla.edu.ec>
2025-12-17 02:50:22 -0500

    refactor: Remove unused `Optional` and `SaveAcademicTermCommand` imports from `AcademicTermQueryUseCase`

--- src/main/java/com/udla/markenx/api/academicterms/application/ports/incoming/AcademicTermQueryUseCase.java
+++ src/main/java/com/udla/markenx/api/academicterms/application/ports/incoming/AcademicTermQueryUseCase.java
@@ -2,14 +2,12 @@ package com.udla.markenx.api.academicterms.application.ports.incoming;
 
 import com.udla.markenx.api.academicterms.application.queries.GetAcademicTermByIdQuery;
 import com.udla.markenx.api.academicterms.application.queries.GetAllAcademicTermsPaginatedQuery;
-import com.udla.markenx.api.academicterms.application.commands.SaveAcademicTermCommand;
 import com.udla.markenx.api.academicterms.domain.models.aggregates.AcademicTerm;
 import com.udla.markenx.api.academicterms.domain.models.valueobjects.AcademicTermStatus;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.data.domain.Page;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface AcademicTermQueryUseCase {
     AcademicTerm getById(GetAcademicTermByIdQuery query);
