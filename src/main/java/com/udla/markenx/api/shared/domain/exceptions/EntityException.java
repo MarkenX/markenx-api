abcecdd4e8c346edba707b30f770160716644318
Christian Jacome <christian.jacome.mora@udla.edu.ec>
2025-12-17 02:57:30 -0500

    refactor: Change `EntityException` constructors to `protected` access modifier

--- src/main/java/com/udla/markenx/api/shared/domain/exceptions/EntityException.java
+++ src/main/java/com/udla/markenx/api/shared/domain/exceptions/EntityException.java
@@ -1,11 +1,11 @@
 package com.udla.markenx.api.shared.domain.exceptions;
 
 public abstract class EntityException extends RuntimeException {
-    public EntityException(String message) {
+    protected EntityException(String message) {
         super(message);
     }
 
-    public EntityException(String message, Throwable throwable) {
+    protected EntityException(String message, Throwable throwable) {
         super(message, throwable);
     }
 }
