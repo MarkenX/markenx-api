789b6f87cc63e7f7ae8f93a0b67392c02c42c863
Christian Jacome <christian.jacome.mora@udla.edu.ec>
2025-12-17 21:30:53 -0500

    chore: Add `Course` aggregate in `com.udla.markenx.api.courses` module and update `@ApplicationModule` annotation

---
+++ src/main/java/com/udla/markenx/api/courses/domain/models/aggregates/Course.java
@@ -0,0 +1,6 @@
+package com.udla.markenx.api.courses.domain.models.aggregates;
+
+import com.udla.markenx.api.shared.domain.models.aggregates.Entity;
+
+public class Course extends Entity {
+}
--- src/main/java/com/udla/markenx/api/courses/package-info.java
+++ src/main/java/com/udla/markenx/api/courses/package-info.java
@@ -1,4 +1,9 @@
-@ApplicationModule(displayName = "Course Management")
+@ApplicationModule(
+        displayName = "Course Management",
+        allowedDependencies = {
+                "shared::aggregates",
+        }
+)
 package com.udla.markenx.api.courses;
 
 import org.springframework.modulith.ApplicationModule;
