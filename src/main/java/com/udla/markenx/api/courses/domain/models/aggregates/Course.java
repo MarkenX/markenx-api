74241893f9ebd5c8b4170f16d010f7ed23f9b698
Christian Jacome <christian.jacome.mora@udla.edu.ec>
2025-12-17 21:49:34 -0500

    chore: Add `InvalidCourseNameException` and name validation to `Course` aggregate

--- src/main/java/com/udla/markenx/api/courses/domain/models/aggregates/Course.java
+++ src/main/java/com/udla/markenx/api/courses/domain/models/aggregates/Course.java
@@ -1,5 +1,6 @@
 package com.udla.markenx.api.courses.domain.models.aggregates;
 
+import com.udla.markenx.api.courses.domain.exceptions.InvalidCourseNameException;
 import com.udla.markenx.api.shared.domain.models.aggregates.Entity;
 import lombok.Getter;
 
@@ -12,13 +13,31 @@ public class Course extends Entity {
 
     public Course(CourseId id, String name, long code) {
         this.id = id;
-        this.name = name;
+        this.name = validateName(name);
         this.code = code;
     }
 
     public Course(String id, String name, long code) {
         this.id = new CourseId(id);
-        this.name = name;
+        this.name = validateName(name);
         this.code = code;
     }
+
+    // region Validations
+
+    /**
+     * Validates the given course name to ensure it is not null or blank.
+     *
+     * @param name the name of the course to validate
+     * @return the validated course name if it passes validation
+     * @throws InvalidCourseNameException if the course name is null or contains only whitespace
+     */
+    public String validateName(String name) {
+        if (name == null || name.isBlank()) {
+            throw new InvalidCourseNameException();
+        }
+        return name;
+    }
+
+    // endregion
 }
