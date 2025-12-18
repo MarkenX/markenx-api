824e3fa9faa0b1d7da49099b69c8b3afb18b2429
Christian Jacome <christian.jacome.mora@udla.edu.ec>
2025-12-17 22:14:35 -0500

    chore: Add `equals` and `hashCode` methods in `Course` aggregate

--- src/main/java/com/udla/markenx/api/courses/domain/models/aggregates/Course.java
+++ src/main/java/com/udla/markenx/api/courses/domain/models/aggregates/Course.java
@@ -97,6 +97,18 @@ public class Course extends Entity {
 
     // endregion
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Course that)) return false;
+        return id.equals(that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id.hashCode();
+    }
+
     /**
      * Formats the course code as a zero-padded four-digit string.
      *
