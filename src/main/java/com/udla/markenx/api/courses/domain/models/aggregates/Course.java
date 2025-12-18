aefafc170d4ae1d2e63592aa2c90770c65977bb5
Christian Jacome <christian.jacome.mora@udla.edu.ec>
2025-12-17 21:34:39 -0500

    chore: Add constructors and `@Getter` annotation to `Course` aggregate

--- src/main/java/com/udla/markenx/api/courses/domain/models/aggregates/Course.java
+++ src/main/java/com/udla/markenx/api/courses/domain/models/aggregates/Course.java
@@ -1,7 +1,18 @@
 package com.udla.markenx.api.courses.domain.models.aggregates;
 
 import com.udla.markenx.api.shared.domain.models.aggregates.Entity;
+import lombok.Getter;
 
+@Getter
 public class Course extends Entity {
 
+    private final CourseId id;
+
+    public Course(CourseId id) {
+        this.id = id;
+    }
+
+    public Course(String id) {
+        this.id = new CourseId(id);
+    }
 }
