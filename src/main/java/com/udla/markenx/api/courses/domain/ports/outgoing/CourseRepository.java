3d1e12c83977bce6e090d6cd57f5cf2b8b16c9ba
Christian Jacome <christian.jacome.mora@udla.edu.ec>
2025-12-18 21:31:51 -0500

    chore: Add `CourseRepository` interface with paginated course query method

---
+++ src/main/java/com/udla/markenx/api/courses/domain/ports/outgoing/CourseRepository.java
@@ -0,0 +1,9 @@
+package com.udla.markenx.api.courses.domain.ports.outgoing;
+
+import com.udla.markenx.api.courses.domain.models.aggregates.Course;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public interface CourseRepository {
+    Page<Course> findAllPaginated(Pageable pageable);
+}
