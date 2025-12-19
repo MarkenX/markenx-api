645be0fc487adfd8b0a1be9105bee8bdb1487662
Christian Jacome <christian.jacome.mora@udla.edu.ec>
2025-12-18 21:56:02 -0500

    chore: Implement `CourseQueryService` with paginated course query functionality

---
+++ src/main/java/com/udla/markenx/api/courses/application/services/CourseQueryService.java
@@ -0,0 +1,24 @@
+package com.udla.markenx.api.courses.application.services;
+
+import com.udla.markenx.api.courses.application.ports.incoming.CourseQueryUseCase;
+import com.udla.markenx.api.courses.application.queries.GetAllCoursesPaginatedQuery;
+import com.udla.markenx.api.courses.domain.models.aggregates.Course;
+import com.udla.markenx.api.courses.domain.ports.outgoing.CourseRepository;
+import lombok.RequiredArgsConstructor;
+import org.jspecify.annotations.NonNull;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CourseQueryService implements CourseQueryUseCase {
+
+    private final CourseRepository repository;
+
+    @Override
+    public Page<Course> getAllPaginated(@NonNull GetAllCoursesPaginatedQuery query) {
+        var pageable = PageRequest.of(query.page(), query.size());
+        return repository.findAllPaginated(pageable);
+    }
+}
