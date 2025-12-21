d7716a00a12f42d1550d2c8f896e4c7c61911da9
Christian Jacome <christian.jacome.mora@udla.edu.ec>
2025-12-20 21:55:41 -0500

    chore: Add Flyway migrations for database schema and academic terms table

---
+++ src/main/resources/db.migrations/V2__create_academic_terms.sql
@@ -0,0 +1,17 @@
+USE markenx;
+
+CREATE TABLE academic_terms (
+    id CHAR(36) NOT NULL,
+    lifecycle_status VARCHAR(20) NOT NULL,
+    start_date DATE NOT NULL,
+    end_date DATE NOT NULL,
+    year INT NOT NULL,
+    sequence INT NOT NULL,
+    status VARCHAR(20) NOT NULL,
+
+    CONSTRAINT pk_academic_terms
+        PRIMARY KEY (id),
+
+    CONSTRAINT uk_academic_terms_year_sequence
+        UNIQUE (year, sequence)
+);
