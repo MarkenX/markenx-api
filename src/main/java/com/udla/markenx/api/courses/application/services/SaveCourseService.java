bb2daa093124c6ce445eecf581b56a225629731b
Christian Jacome <christian.jacome.mora@udla.edu.ec>
2025-12-20 22:03:26 -0500

    chore: Replace `CourseRepository` with `CourseCommandRepository` in `SaveCourseService`

--- src/main/java/com/udla/markenx/api/courses/application/services/SaveCourseService.java
+++ src/main/java/com/udla/markenx/api/courses/application/services/SaveCourseService.java
@@ -1,12 +1,10 @@
 package com.udla.markenx.api.courses.application.services;
 
 import com.udla.markenx.api.academicterms.application.ports.incoming.UpdateAcademicTermUseCase;
-import com.udla.markenx.api.academicterms.application.queries.GetAcademicTermByIdQuery;
-import com.udla.markenx.api.academicterms.domain.models.aggregates.AcademicTerm;
 import com.udla.markenx.api.courses.application.commands.SaveCourseCommand;
 import com.udla.markenx.api.courses.application.ports.incoming.SaveCourseUseCase;
 import com.udla.markenx.api.courses.domain.models.aggregates.Course;
-import com.udla.markenx.api.courses.domain.ports.outgoing.CourseRepository;
+import com.udla.markenx.api.courses.domain.ports.outgoing.CourseCommandRepository;
 import lombok.RequiredArgsConstructor;
 import org.jspecify.annotations.NonNull;
 import org.springframework.stereotype.Service;
