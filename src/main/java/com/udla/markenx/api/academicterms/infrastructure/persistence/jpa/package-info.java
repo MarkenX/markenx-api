bcee0eb93bdcb0d4fdf5609d1b2868bc25ed74d1
Christian Jacome <christian.jacome.mora@udla.edu.ec>
2025-12-20 21:56:42 -0500

    chore: Remove JPA repositories, entities, and related dependencies for academic terms and courses

--- src/main/java/com/udla/markenx/api/academicterms/infrastructure/persistence/jpa/package-info.java
+++
@@ -1,4 +0,0 @@
-@NamedInterface("persistence-jpa")
-package com.udla.markenx.api.academicterms.infrastructure.persistence.jpa;
-
-import org.springframework.modulith.NamedInterface;
--- src/main/java/com/udla/markenx/api/courses/infrastructure/persistence/jpa/SpringDataCourseRepository.java
+++
@@ -1,7 +0,0 @@
-package com.udla.markenx.api.courses.infrastructure.persistence.jpa;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SpringDataCourseRepository extends JpaRepository<CourseJpaEntity, String> {
-
-}
