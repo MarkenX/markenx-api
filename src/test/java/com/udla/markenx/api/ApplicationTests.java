fbe2b6f4848925a4b9e4a0aa87d7f9f5a99b2853
Christian Jacome <christian.jacome.mora@udla.edu.ec>
2025-12-20 23:28:01 -0500

    chore: Integrate Testcontainers with MySQL for testing and configure dynamic datasource properties

--- src/test/java/com/udla/markenx/api/ApplicationTests.java
+++ src/test/java/com/udla/markenx/api/ApplicationTests.java
@@ -3,13 +3,34 @@ package com.udla.markenx.api;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 
 @SpringBootTest
 @ActiveProfiles("test")
+@Testcontainers
 class ApplicationTests {
+
+	@Container
+	static MySQLContainer<?> mysql =
+			new MySQLContainer<>("mysql:8.0")
+					.withDatabaseName("markenx")
+					.withUsername("test")
+					.withPassword("test");
+
+	@DynamicPropertySource
+	static void datasourceProps(DynamicPropertyRegistry registry) {
+		registry.add("spring.datasource.url", mysql::getJdbcUrl);
+		registry.add("spring.datasource.username", mysql::getUsername);
+		registry.add("spring.datasource.password", mysql::getPassword);
+		registry.add("spring.datasource.driver-class-name",
+				() -> "com.mysql.cj.jdbc.Driver");
+	}
 
 	@Test
 	void contextLoads() {
 	}
-
 }
