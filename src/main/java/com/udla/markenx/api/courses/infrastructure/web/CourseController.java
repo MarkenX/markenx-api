079cb35833345217176c49662dc753d99dd50a62
Christian Jacome <christian.jacome.mora@udla.edu.ec>
2025-12-21 00:57:30 -0500

    chore: Rename `CourseController` endpoint from `academic-terms` to `courses`

--- src/main/java/com/udla/markenx/api/courses/infrastructure/web/CourseController.java
+++ src/main/java/com/udla/markenx/api/courses/infrastructure/web/CourseController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("academic-terms")
+@RequestMapping("courses")
 public class CourseController {
 
     private final CourseDTOMapper mapper;
