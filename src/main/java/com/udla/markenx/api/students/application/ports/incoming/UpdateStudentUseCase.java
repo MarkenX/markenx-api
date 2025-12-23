78f62c8115c088785fef3d375275f63e87bc953f
Christian Jacome <christian.jacome.mora@udla.edu.ec>
2025-12-23 01:21:49 -0500

    chore: Add UpdateStudentUseCase interface for retrieving students by ID

---
+++ src/main/java/com/udla/markenx/api/students/application/ports/incoming/UpdateStudentUseCase.java
@@ -0,0 +1,8 @@
+package com.udla.markenx.api.students.application.ports.incoming;
+
+import com.udla.markenx.api.students.application.queries.GetStudentByIdQuery;
+import com.udla.markenx.api.students.domain.models.aggregates.Student;
+
+public interface UpdateStudentUseCase {
+    Student getById(GetStudentByIdQuery query);
+}
