5eda7ef74f4eb39f1970ca8a98dbeb273e53c99e
Christian Jacome <christian.jacome.mora@udla.edu.ec>
2025-12-22 23:30:43 -0500

    chore: Add EmailDomainConfig component to manage allowed email domains

---
+++ src/main/java/com/udla/markenx/api/students/infrastructure/email/EmailDomainConfig.java
@@ -0,0 +1,22 @@
+package com.udla.markenx.api.students.infrastructure.email;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.Set;
+
+@Component
+public class EmailDomainConfig {
+
+    private final Set<String> allowedDomains;
+
+    public EmailDomainConfig(
+            @Value("${email.allowed-domains}") String domains
+    ) {
+        this.allowedDomains = Set.of(domains.toLowerCase().split(","));
+    }
+
+    public Set<String> allowedDomains() {
+        return allowedDomains;
+    }
+}
