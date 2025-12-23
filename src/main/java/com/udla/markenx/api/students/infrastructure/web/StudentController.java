9932e5117ceb898a5001ef3792552ffedb3a7a61
Christian Jacome <christian.jacome.mora@udla.edu.ec>
2025-12-23 01:56:54 -0500

    feat: Add get all students endpoint with pagination in StudentController

--- src/main/java/com/udla/markenx/api/students/infrastructure/web/StudentController.java
+++ src/main/java/com/udla/markenx/api/students/infrastructure/web/StudentController.java
@@ -5,11 +5,16 @@ import com.udla.markenx.api.students.application.dtos.CreateStudentRequestDTO;
 import com.udla.markenx.api.students.application.dtos.StudentResponseDTO;
 import com.udla.markenx.api.students.application.mappers.StudentDTOMapper;
 import com.udla.markenx.api.students.application.ports.incoming.SaveStudentUseCase;
+import com.udla.markenx.api.students.application.ports.incoming.StudentQueryUseCase;
+import com.udla.markenx.api.students.application.queries.GetAllStudentsPaginatedQuery;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -19,6 +24,7 @@ public class StudentController {
 
     private final StudentDTOMapper mapper;
     private final SaveStudentUseCase saveTermUseCase;
+    private final StudentQueryUseCase studentQueryUseCase;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
@@ -30,5 +36,26 @@ public class StudentController {
         var command = new SaveStudentCommand(
                 dto.firstName(), dto.lastName(), dto.email(), dto.courseId());
         return mapper.toDTO(saveTermUseCase.handle(command));
+    }
+
+    @GetMapping
+    @Operation(summary = "Get all students")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Students retrieved successfully"),
+            @ApiResponse(responseCode = "404", description = "No students found")
+    })
+    public ResponseEntity<@NotNull Page<@NotNull StudentResponseDTO>> getAll(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size
+    ) {
+        var query = new GetAllStudentsPaginatedQuery(page, size);
+        Page<@NotNull StudentResponseDTO> result =
+                studentQueryUseCase.getAllPaginated(query).map(mapper::toDTO);
+
+        if (result.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        return ResponseEntity.ok(result);
     }
 }
