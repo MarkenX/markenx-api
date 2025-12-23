b47a354ff813f453edba52d0cf8a6073e45aa800
Christian Jacome <christian.jacome.mora@udla.edu.ec>
2025-12-23 01:25:06 -0500

    chore: Create students table migration script

---
+++ src/main/resources/db/migration/V3__create_students.sql
@@ -0,0 +1,24 @@
+CREATE TABLE students (
+     id CHAR(36) NOT NULL,
+     lifecycle_status VARCHAR(20) NOT NULL,
+     first_name VARCHAR(255) NOT NULL,
+     last_name VARCHAR(255) NOT NULL,
+     email VARCHAR(255) NOT NULL,
+     code INT NOT NULL AUTO_INCREMENT,
+     course_id CHAR(36) NOT NULL,
+
+     CONSTRAINT pk_courses
+         PRIMARY KEY (id),
+
+     CONSTRAINT uk_courses_code
+         UNIQUE (code),
+
+     CONSTRAINT fk_students_course
+         FOREIGN KEY (course_id)
+             REFERENCES courses (id)
+             ON DELETE RESTRICT
+             ON UPDATE CASCADE
+);
+
+CREATE INDEX idx_students_course
+    ON students (course_id);
