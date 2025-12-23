17b5eb8c290d73dfac3d736ff2128998aaa75cd5
Christian Jacome <christian.jacome.mora@udla.edu.ec>
2025-12-23 01:47:46 -0500

    chore: Add StudentController for handling student-related HTTP requests

---
+++ src/main/java/com/udla/markenx/api/students/infrastructure/web/StudentController.java
@@ -0,0 +1,11 @@
+package com.udla.markenx.api.students.infrastructure.web;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("students")
+public class StudentController {
+}
