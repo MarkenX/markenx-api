c39c6417b23841b43e82f29c8d977e83177c8e4d
Christian Jacome <christian.jacome.mora@udla.edu.ec>
2025-12-23 01:37:35 -0500

    chore: Add JdbcStudentRepository for saving and retrieving students from the database

---
+++ src/main/java/com/udla/markenx/api/students/infrastructure/persistance/jdbc/JdbcStudentRepository.java
@@ -0,0 +1,58 @@
+package com.udla.markenx.api.students.infrastructure.persistance.jdbc;
+
+import com.udla.markenx.api.students.application.exceptions.StudentNotFoundException;
+import com.udla.markenx.api.students.domain.models.aggregates.Student;
+import com.udla.markenx.api.students.domain.ports.outgoing.StudentCommandRepository;
+import lombok.RequiredArgsConstructor;
+import org.jspecify.annotations.NonNull;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class JdbcStudentRepository implements StudentCommandRepository {
+
+    private final StudentRowMapper rowMapper = new StudentRowMapper();
+    private final JdbcTemplate jdbcTemplate;
+
+    @Override
+    public Student save(@NonNull Student student) {
+        jdbcTemplate.update("""
+            INSERT INTO students
+            (id, lifecycle_status, first_name, last_name, email, course_id)
+            VALUES (?, ?, ?, ?, ?, ?)
+            """,
+                student.getId(),
+                student.getLifecycleStatus().name(),
+                student.getFirstName(),
+                student.getLastName(),
+                student.getEmail().getValue(),
+                student.getCourseId()
+        );
+
+        return jdbcTemplate.queryForObject("""
+                SELECT *
+                FROM students
+                WHERE id = ?
+                """,
+                rowMapper,
+                student.getId());
+    }
+
+    @Override
+    public Student findById(String id) {
+        try {
+            return jdbcTemplate.queryForObject("""
+            SELECT *
+            FROM students
+            WHERE id = ?
+            """,
+                rowMapper,
+                id
+            );
+        } catch (EmptyResultDataAccessException ex) {
+            throw new StudentNotFoundException(id);
+        }
+    }
+}
