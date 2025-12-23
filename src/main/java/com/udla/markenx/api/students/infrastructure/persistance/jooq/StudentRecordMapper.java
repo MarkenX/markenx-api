6341b7fd46c674ff2ed61546fbeaae6ffa9f3cd4
Christian Jacome <christian.jacome.mora@udla.edu.ec>
2025-12-23 01:40:20 -0500

    chore: Add StudentRecordMapper for mapping database records to Student domain entities

---
+++ src/main/java/com/udla/markenx/api/students/infrastructure/persistance/jooq/StudentRecordMapper.java
@@ -0,0 +1,21 @@
+package com.udla.markenx.api.students.infrastructure.persistance.jooq;
+
+import com.udla.markenx.api.shared.domain.models.valueobjects.LifecycleStatus;
+import com.udla.markenx.api.students.domain.models.aggregates.Student;
+import org.jooq.Record;
+import org.jspecify.annotations.NonNull;
+
+public class StudentRecordMapper {
+
+    public Student toDomain(@NonNull Record r) {
+        return new Student(
+                r.get("id", String.class),
+                LifecycleStatus.valueOf(r.get("lifecycle_status", String.class)),
+                r.get("code", Long.class),
+                r.get("first_name", String.class),
+                r.get("last_name", String.class),
+                r.get("email", String.class),
+                r.get("course_id", String.class)
+        );
+    }
+}
