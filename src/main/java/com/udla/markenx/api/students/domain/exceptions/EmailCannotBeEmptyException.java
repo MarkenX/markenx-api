9e95c0522f3a00e8db0858b8f7b0fcc666f5b777
Christian Jacome <christian.jacome.mora@udla.edu.ec>
2025-12-22 23:30:51 -0500

    chore: Add custom email exceptions to students domain

---
+++ src/main/java/com/udla/markenx/api/students/domain/exceptions/EmailCannotBeEmptyException.java
@@ -0,0 +1,7 @@
+package com.udla.markenx.api.students.domain.exceptions;
+
+public class EmailCannotBeEmptyException extends EmailException {
+    public EmailCannotBeEmptyException() {
+        super("El correo electrónico no puede ser nulo ni estar vacío");
+    }
+}
---
+++ src/main/java/com/udla/markenx/api/students/domain/exceptions/EmailException.java
@@ -0,0 +1,11 @@
+package com.udla.markenx.api.students.domain.exceptions;
+
+public abstract class EmailException extends RuntimeException {
+    public EmailException(String message) {
+        super(message);
+    }
+
+    public EmailException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
