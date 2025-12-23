5ea11f6ad365265e6191cd0ed5c9a5d638972137
Christian Jacome <christian.jacome.mora@udla.edu.ec>
2025-12-22 22:40:49 -0500

    chore: Add Student aggregate and update allowed dependencies in Student Management module

---
+++ src/main/java/com/udla/markenx/api/students/domain/models/aggregates/Student.java
@@ -0,0 +1,6 @@
+package com.udla.markenx.api.students.domain.models.aggregates;
+
+import com.udla.markenx.api.shared.domain.models.aggregates.Entity;
+
+public class Student extends Entity {
+}
--- src/main/java/com/udla/markenx/api/students/package-info.java
+++ src/main/java/com/udla/markenx/api/students/package-info.java
@@ -1,4 +1,9 @@
-@ApplicationModule(displayName = "Student Management")
+@ApplicationModule(
+    displayName = "Student Management",
+    allowedDependencies = {
+        "shared::aggregates",
+    }
+)
 package com.udla.markenx.api.students;
 
 import org.springframework.modulith.ApplicationModule;
