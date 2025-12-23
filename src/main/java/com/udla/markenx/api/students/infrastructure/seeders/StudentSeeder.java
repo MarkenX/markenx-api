e17cf6618dc7bc0558f5b8481c1708f50e4ca1d5
Christian Jacome <christian.jacome.mora@udla.edu.ec>
2025-12-23 01:45:29 -0500

    chore: Add StudentSeeder for seeding data in the dev profile

---
+++ src/main/java/com/udla/markenx/api/students/infrastructure/seeders/StudentSeeder.java
@@ -0,0 +1,25 @@
+package com.udla.markenx.api.students.infrastructure.seeders;
+
+import com.udla.markenx.api.students.application.ports.incoming.SaveStudentUseCase;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.flywaydb.core.Flyway;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@Profile("dev")
+@Order(3)
+@RequiredArgsConstructor
+public class StudentSeeder implements CommandLineRunner {
+
+    private final Flyway flyway;
+
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
