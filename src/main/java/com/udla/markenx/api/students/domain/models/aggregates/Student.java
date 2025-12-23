eb08a646ea26de19da0e61e50d0fa37da3b7767c
Christian Jacome <christian.jacome.mora@udla.edu.ec>
2025-12-22 22:43:00 -0500

    chore: Add @Getter annotation and constructor to Student aggregate for id initialization

--- src/main/java/com/udla/markenx/api/students/domain/models/aggregates/Student.java
+++ src/main/java/com/udla/markenx/api/students/domain/models/aggregates/Student.java
@@ -1,6 +1,14 @@
 package com.udla.markenx.api.students.domain.models.aggregates;
 
 import com.udla.markenx.api.shared.domain.models.aggregates.Entity;
+import lombok.Getter;
 
+@Getter
 public class Student extends Entity {
+
+    private final StudentId id;
+
+    public Student(StudentId id) {
+        this.id = id;
+    }
 }
