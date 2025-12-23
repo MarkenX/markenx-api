b5af6ed0189c98731bf3484dde0d0037b42ddbdb
Christian Jacome <christian.jacome.mora@udla.edu.ec>
2025-12-23 01:54:52 -0500

    chore: Add StudentDTOMapper for converting Student domain to StudentResponseDTO

---
+++ src/main/java/com/udla/markenx/api/students/application/mappers/StudentDTOMapper.java
@@ -0,0 +1,20 @@
+package com.udla.markenx.api.students.application.mappers;
+
+import com.udla.markenx.api.students.application.dtos.StudentResponseDTO;
+import com.udla.markenx.api.students.domain.models.aggregates.Student;
+import org.jspecify.annotations.NonNull;
+import org.springframework.stereotype.Component;
+
+@Component
+public class StudentDTOMapper {
+
+    public StudentResponseDTO toDTO(@NonNull Student domain) {
+        return new StudentResponseDTO(
+                domain.getId(),
+                domain.toString(),
+                domain.getFullName(),
+                domain.getEmail().getValue(),
+                domain.getCourseId()
+        );
+    }
+}
