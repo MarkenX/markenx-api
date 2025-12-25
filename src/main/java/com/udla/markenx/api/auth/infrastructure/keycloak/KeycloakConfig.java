874179ef2bdccbf9f90a04e4a9df8ef9c760cadd
Christian Jacome <christian.jacome.mora@udla.edu.ec>
2025-12-25 01:24:45 -0500

    refactor: Make `KeycloakConfig` class public

--- src/main/java/com/udla/markenx/api/auth/infrastructure/keycloak/KeycloakConfig.java
+++ src/main/java/com/udla/markenx/api/auth/infrastructure/keycloak/KeycloakConfig.java
@@ -5,4 +5,4 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableConfigurationProperties(KeycloakProperties.class)
-class KeycloakConfig {}
+public class KeycloakConfig {}
