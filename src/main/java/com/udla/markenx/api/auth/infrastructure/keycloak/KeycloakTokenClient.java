6e670c534e20986a6415aad2c2148d8efc4f3e69
Christian Jacome <christian.jacome.mora@udla.edu.ec>
2025-12-25 19:58:48 -0500

    feat: Add WebClient integration for Keycloak token retrieval and update dependencies in pom.xml

---
+++ src/main/java/com/udla/markenx/api/auth/infrastructure/keycloak/KeycloakTokenClient.java
@@ -0,0 +1,35 @@
+package com.udla.markenx.api.auth.infrastructure.keycloak;
+
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+@Component
+public class KeycloakTokenClient {
+
+    private final WebClient webClient;
+    private final KeycloakProperties props;
+
+    KeycloakTokenClient(WebClient.Builder builder, KeycloakProperties props) {
+        this.props = props;
+        this.webClient = builder.baseUrl(props.baseUrl()).build();
+    }
+
+    public Mono<String> getAccessToken() {
+        return webClient.post()
+                .uri("/realms/{realm}/protocol/openid-connect/token", props.realm())
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .body(BodyInserters.fromFormData("grant_type", "client_credentials")
+                        .with("client_id", props.clientId())
+                        .with("client_secret", props.clientSecret()))
+                .retrieve()
+                .bodyToMono(TokenResponse.class)
+                .map(TokenResponse::accessToken);
+    }
+
+    record TokenResponse(String access_token) {
+        String accessToken() { return access_token; }
+    }
+}
