9dd2a1ed79585768be1314e0e5c028a94f6c73e0
Christian Jacome <christian.jacome.mora@udla.edu.ec>
2025-12-27 08:14:38 -0500

    feat: Add `UserId` and `Role` value objects to `User` aggregate for enhanced domain modeling

--- src/main/java/com/udla/markenx/api/users/domain/models/aggregates/User.java
+++ src/main/java/com/udla/markenx/api/users/domain/models/aggregates/User.java
@@ -1,13 +1,34 @@
 package com.udla.markenx.api.users.domain.models.aggregates;
 
+import com.udla.markenx.api.shared.domain.models.aggregates.Entity;
+import com.udla.markenx.api.shared.domain.models.valueobjects.LifecycleStatus;
 import com.udla.markenx.api.users.domain.models.valueobjects.Email;
+import com.udla.markenx.api.users.domain.models.valueobjects.Role;
 
-public class User {
+@SuppressWarnings("LombokGetterMayBeUsed")
+public class User extends Entity {
 
+    private final UserId id;
     private Email email;
+    private final Role role;
+
+    private User(String id, LifecycleStatus status, Email email, Role role) {
+        super(status);
+        this.id = new UserId(id);
+        this.email = email;
+        this.role = role;
+    }
 
     public void updateEmail(Email email) {
         this.email = email;
+    }
+
+    public String getId() {
+        return this.id.value();
+    }
+
+    public Role getRole() {
+        return role;
     }
 
     public Email getEmail() {
