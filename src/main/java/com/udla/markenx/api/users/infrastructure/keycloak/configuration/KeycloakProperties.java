72151997e28d95936fdd532306816983acb285dc
Christian Jacome <christian.jacome.mora@udla.edu.ec>
2025-12-27 16:38:55 -0500

    refactor: Update `KeycloakProperties` to replace `baseUrl` with `scheme`, `host`, and `port` attributes

--- src/main/java/com/udla/markenx/api/users/infrastructure/keycloak/configuration/KeycloakProperties.java
+++ src/main/java/com/udla/markenx/api/users/infrastructure/keycloak/configuration/KeycloakProperties.java
@@ -4,7 +4,9 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties(prefix = "keycloak")
 public record KeycloakProperties(
-        String baseUrl,
+        String scheme,
+        String host,
+        int port,
         String realm,
         String clientId,
         String clientSecret
