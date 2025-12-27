499b9353a54dc7ddff351545887794fc1f5bc4cf
Christian Jacome <christian.jacome.mora@udla.edu.ec>
2025-12-27 00:40:12 -0500

    chore: Remove unused StudentIdentityProvider interface

--- src/main/java/com/udla/markenx/api/students/domain/ports/outgoing/StudentIdentityProvider.java
+++
@@ -1,7 +0,0 @@
-package com.udla.markenx.api.students.domain.ports.outgoing;
-
-import com.udla.markenx.api.students.domain.models.aggregates.Student;
-
-public interface StudentIdentityProvider {
-    void createUser(Student student);
-}
