ab319405a2083d9cc94dd8bfe3759ffa28fc2374
Christian Jacome <christian.jacome.mora@udla.edu.ec>
2025-12-27 08:37:02 -0500

    refactor: Replace `UserQueryRepository` with `UserCommandRepository` and update `JdbcUserRepository` implementation

--- src/main/java/com/udla/markenx/api/users/domain/ports/outgoing/UserCommandRepository.java
+++ src/main/java/com/udla/markenx/api/users/domain/ports/outgoing/UserCommandRepository.java
@@ -2,5 +2,7 @@ package com.udla.markenx.api.users.domain.ports.outgoing;
 
 import com.udla.markenx.api.users.domain.models.aggregates.User;
 
-public interface UserQueryRepository {
+public interface UserCommandRepository {
+    User save(User user);
+    User findById(String id);
 }
--- src/main/java/com/udla/markenx/api/users/infrastructure/persistence/jdbc/JdbcUserRepository.java
+++ src/main/java/com/udla/markenx/api/users/infrastructure/persistence/jdbc/JdbcUserRepository.java
@@ -1,13 +1,18 @@
 package com.udla.markenx.api.users.infrastructure.persistence.jdbc;
 
 import com.udla.markenx.api.users.domain.models.aggregates.User;
-import com.udla.markenx.api.users.domain.ports.outgoing.UserQueryRepository;
+import com.udla.markenx.api.users.domain.ports.outgoing.UserCommandRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 @Repository
 @RequiredArgsConstructor
-public class JdbcUserRepository implements UserQueryRepository {
+public class JdbcUserRepository implements UserCommandRepository {
+
+    @Override
+    public User save(User user) {
+        return null;
+    }
 
     @Override
     public User findById(String id) {
