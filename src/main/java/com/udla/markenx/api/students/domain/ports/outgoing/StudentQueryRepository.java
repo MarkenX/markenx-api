802567c4c773fe000bdd0270cb53f7c80ef1920b
Christian Jacome <christian.jacome.mora@udla.edu.ec>
2025-12-27 00:40:39 -0500

    feat: Add `findById` method to `StudentQueryRepository` interface

--- src/main/java/com/udla/markenx/api/students/domain/ports/outgoing/StudentQueryRepository.java
+++ src/main/java/com/udla/markenx/api/students/domain/ports/outgoing/StudentQueryRepository.java
@@ -1,9 +1,11 @@
 package com.udla.markenx.api.students.domain.ports.outgoing;
 
+import com.udla.markenx.api.academicterms.domain.models.aggregates.AcademicTerm;
 import com.udla.markenx.api.students.domain.models.aggregates.Student;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 public interface StudentQueryRepository {
+    Student findById(String id);
     Page<Student> findAllPaginated(Pageable pageable);
 }
