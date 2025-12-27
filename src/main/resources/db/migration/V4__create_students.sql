b41cdfc9893cc574f234abf3c451f022b04847e4
Christian Jacome <christian.jacome.mora@udla.edu.ec>
2025-12-27 08:27:59 -0500

    feat: Introduce `users` table and update `students` table with `user_id` column and constraints

--- src/main/resources/db/migration/V4__create_students.sql
+++ src/main/resources/db/migration/V4__create_students.sql
@@ -5,7 +5,7 @@ CREATE TABLE students (
     last_name VARCHAR(255) NOT NULL,
     code INT NOT NULL AUTO_INCREMENT,
     course_id CHAR(36) NOT NULL,
-    student_id CHAR(36) NOT NULL,
+    user_id CHAR(36) NOT NULL,
 
     CONSTRAINT pk_courses
         PRIMARY KEY (id),
@@ -17,8 +17,17 @@ CREATE TABLE students (
         FOREIGN KEY (course_id)
             REFERENCES courses (id)
                 ON DELETE RESTRICT
+                ON UPDATE CASCADE,
+
+    CONSTRAINT fk_students_user
+        FOREIGN KEY (user_id)
+            REFERENCES users (id)
+                ON DELETE RESTRICT
                 ON UPDATE CASCADE
 );
 
 CREATE INDEX idx_students_course
     ON students (course_id);
+
+CREATE INDEX idx_students_user
+    ON students (user_id);
