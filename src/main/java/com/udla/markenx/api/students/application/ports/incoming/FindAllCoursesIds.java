8ddbc481e5121907cf0c398f12d71932e69c8217
Christian Jacome <christian.jacome.mora@udla.edu.ec>
2025-12-28 01:29:12 -0500

    feat: Add student seeding functionality with course integration and enhanced repository support

---
+++ src/main/java/com/udla/markenx/api/students/application/ports/incoming/FindAllCoursesIds.java
@@ -0,0 +1,7 @@
+package com.udla.markenx.api.students.application.ports.incoming;
+
+import java.util.List;
+
+public interface FindAllCoursesIds {
+    List<String> findAllIds();
+}
--- src/main/java/com/udla/markenx/api/students/infrastructure/seeders/StudentSeeder.java
+++ src/main/java/com/udla/markenx/api/students/infrastructure/seeders/StudentSeeder.java
@@ -1,13 +1,20 @@
 package com.udla.markenx.api.students.infrastructure.seeders;
 
+import com.udla.markenx.api.students.application.commands.SaveStudentCommand;
+import com.udla.markenx.api.students.application.ports.incoming.FindAllCoursesIds;
 import com.udla.markenx.api.students.application.ports.incoming.SaveStudentUseCase;
+import com.udla.markenx.api.students.domain.exceptions.StudentException;
+import com.udla.markenx.api.students.domain.models.aggregates.Student;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.flywaydb.core.Flyway;
+import org.jspecify.annotations.NonNull;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Profile;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Slf4j
 @Component
@@ -16,10 +23,27 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class StudentSeeder implements CommandLineRunner {
 
+    private final FindAllCoursesIds findAllCoursesIds;
+    private final SaveStudentUseCase saveStudentUseCase;
     private final Flyway flyway;
 
     @Override
-    public void run(String... args) throws Exception {
+    public void run(String @NonNull ... args) {
+        log.info("Seeding students...");
 
+        List<String> coursesIds = findAllCoursesIds.findAllIds();
+
+        try {
+            coursesIds.forEach(courseId -> {
+               var query = new SaveStudentCommand(
+                       "John", "Doe", courseId, "test@udla.edu.ec");
+                Student saved = saveStudentUseCase.handle(query);
+                log.info("The student {} was created", saved.toString());
+            });
+            log.info("Students seeded successfully.");
+        } catch (StudentException e) {
+            log.error(e.getMessage(), e);
+            log.info("Students seeding failed.");
+        }
     }
 }
