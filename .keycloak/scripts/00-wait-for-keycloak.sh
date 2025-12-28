0dd5a46ba6b09361a2078ba89a94bf82b885236c
Christian Jacome <christian.jacome.mora@udla.edu.ec>
2025-12-27 23:51:02 -0500

    chore: Remove deprecated Keycloak initialization scripts

--- .keycloak/scripts/00-wait-for-keycloak.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-KEYCLOAK_BASE_URL="${KEYCLOAK_SCHEME}://${KEYCLOAK_HOST}:${KEYCLOAK_PORT}"
-
-echo "Waiting for Keycloak at ${KEYCLOAK_BASE_URL}..."
-until curl -sf "${KEYCLOAK_BASE_URL}/health/ready"; do
-  sleep 2
-done
