3599f77d3bb762f79d2280a320c1977d7c28d7ea
Christian Jacome <christian.jacome.mora@udla.edu.ec>
2026-01-02 13:49:15 -0500

    feat: Update `StudentQueryService` to use `StudentSummaryPagedReadRepository` and return `StudentSummaryReadModel`

--- src/main/java/com/udla/markenx/api/students/application/services/StudentQueryService.java
+++ src/main/java/com/udla/markenx/api/students/application/services/StudentQueryService.java
@@ -4,6 +4,9 @@ import com.udla.markenx.api.students.infrastructure.web.rest.dtos.StudentUserRea
 import com.udla.markenx.api.students.application.ports.incoming.StudentQueryUseCase;
 import com.udla.markenx.api.students.application.queries.GetAllStudentsPaginatedQuery;
 import com.udla.markenx.api.students.domain.ports.outgoing.StudentQueryRepository;
+import com.udla.markenx.api.students.query.models.StudentSummaryReadModel;
+import com.udla.markenx.api.students.query.repositories.StudentSummaryPagedReadRepository;
+import com.udla.markenx.api.students.query.repositories.StudentSummaryReadRepository;
 import lombok.RequiredArgsConstructor;
 import org.jspecify.annotations.NonNull;
 import org.springframework.data.domain.Page;
@@ -14,10 +17,10 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class StudentQueryService implements StudentQueryUseCase {
 
-    private final StudentQueryRepository repository;
+    private final StudentSummaryPagedReadRepository repository;
 
     @Override
-    public Page<StudentUserReadDTO> getAllPaginated(@NonNull GetAllStudentsPaginatedQuery query) {
+    public Page<StudentSummaryReadModel> getAllPaginated(@NonNull GetAllStudentsPaginatedQuery query) {
         var pageable = PageRequest.of(query.page(), query.size());
         return repository.findAllPaginated(pageable);
     }
