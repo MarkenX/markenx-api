b850170682f0ca5fb6761c6ad3a75a408736c6fa
Christian Jacome <christian.jacome.mora@udla.edu.ec>
2026-01-02 13:49:34 -0500

    refactor: Rename `userId` to `fullName` in `StudentUserReadDTO` to improve clarity

--- src/main/java/com/udla/markenx/api/students/infrastructure/web/rest/dtos/StudentUserReadDTO.java
+++ src/main/java/com/udla/markenx/api/students/infrastructure/web/rest/dtos/StudentUserReadDTO.java
@@ -2,7 +2,7 @@ package com.udla.markenx.api.students.infrastructure.web.rest.dtos;
 
 public record StudentUserReadDTO(
     String studentId,
-    String userId,
+    String fullName,
     String email
 ) {
 }
