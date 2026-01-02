196bf5061f782c66c7946560b238983aa9d5515c
Christian Jacome <christian.jacome.mora@udla.edu.ec>
2026-01-02 10:47:28 -0500

    refactor: Replace `findById` with `deleteById` in UserCommandRepository interface

--- src/main/java/com/udla/markenx/api/users/domain/ports/outgoing/UserCommandRepository.java
+++ src/main/java/com/udla/markenx/api/users/domain/ports/outgoing/UserCommandRepository.java
@@ -4,5 +4,5 @@ import com.udla.markenx.api.users.domain.models.aggregates.User;
 
 public interface UserCommandRepository {
     User save(User user);
-    User findById(String id);
+    void deleteById(String id);
 }
