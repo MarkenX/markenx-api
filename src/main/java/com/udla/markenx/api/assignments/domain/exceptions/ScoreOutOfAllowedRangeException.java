c59e30b421c5a281c634d6e40e176a5c2586f8a7
Christian Jacome <christian.jacome.mora@udla.edu.ec>
2026-01-04 01:08:04 -0500

    feat: Add ScoreOutOfAllowedRangeException for handling score range validation

---
+++ src/main/java/com/udla/markenx/api/assignments/domain/exceptions/ScoreOutOfAllowedRangeException.java
@@ -0,0 +1,8 @@
+package com.udla.markenx.api.assignments.domain.exceptions;
+
+public class ScoreOutOfAllowedRangeException extends AssignmentException {
+
+    public ScoreOutOfAllowedRangeException() {
+        super("El puntaje mínimo para aprobar debe estar entre 0 y 1.");
+    }
+}
