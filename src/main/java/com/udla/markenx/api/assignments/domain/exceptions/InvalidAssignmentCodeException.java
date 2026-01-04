47bb3983ec3f24a0d82d00520ff85b7883201e85
Christian Jacome <christian.jacome.mora@udla.edu.ec>
2026-01-03 21:27:10 -0500

    feat: Update InvalidAssignmentCodeException constructor to use default message

--- src/main/java/com/udla/markenx/api/assignments/domain/exceptions/InvalidAssignmentCodeException.java
+++ src/main/java/com/udla/markenx/api/assignments/domain/exceptions/InvalidAssignmentCodeException.java
@@ -1,7 +1,7 @@
 package com.udla.markenx.api.assignments.domain.exceptions;
 
 public class InvalidAssignmentCodeException extends AssignmentException {
-    public InvalidAssignmentCodeException(String message) {
+    public InvalidAssignmentCodeException() {
         super("El código no puede ser un número negativo o cero");
     }
 }
