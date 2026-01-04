893eaa8114ea171c61c51d08c21b99c6a3549745
Christian Jacome <christian.jacome.mora@udla.edu.ec>
2026-01-04 01:08:14 -0500

    feat: Add validation to AssignmentScore for score range and NaN handling

--- src/main/java/com/udla/markenx/api/assignments/domain/models/valueobjects/AssignmentScore.java
+++ src/main/java/com/udla/markenx/api/assignments/domain/models/valueobjects/AssignmentScore.java
@@ -1,4 +1,15 @@
 package com.udla.markenx.api.assignments.domain.models.valueobjects;
 
-public record AssignmentScore() {
+import com.udla.markenx.api.assignments.domain.exceptions.ScoreIsNotANumberException;
+import com.udla.markenx.api.assignments.domain.exceptions.ScoreOutOfAllowedRangeException;
+
+public record AssignmentScore(Double value) {
+    public AssignmentScore {
+        if (Double.isNaN(value)) {
+            throw new ScoreIsNotANumberException();
+        }
+        if (value < 0 || value > 1) {
+            throw new ScoreOutOfAllowedRangeException();
+        }
+    }
 }
