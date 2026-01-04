a6be2643c2000d1b1e65a7e095b35226abba05a3
Christian Jacome <christian.jacome.mora@udla.edu.ec>
2026-01-04 09:08:07 -0500

    feat: Add save and findById methods to AssignmentCommandRepository for task management

--- src/main/java/com/udla/markenx/api/assignments/domain/ports/outgoing/AssignmentCommandRepository.java
+++ src/main/java/com/udla/markenx/api/assignments/domain/ports/outgoing/AssignmentCommandRepository.java
@@ -1,4 +1,8 @@
 package com.udla.markenx.api.assignments.domain.ports.outgoing;
 
+import com.udla.markenx.api.assignments.domain.models.aggregates.Task;
+
 public interface AssignmentCommandRepository {
+    Task save(Task task);
+    Task findById(String id);
 }
