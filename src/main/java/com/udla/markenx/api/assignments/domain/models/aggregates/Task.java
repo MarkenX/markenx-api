f6bd29d86547eab53ef0de74c46a9ea01a895a9a
Christian Jacome <christian.jacome.mora@udla.edu.ec>
2026-01-04 01:13:12 -0500

    feat: Add minScoreToPass parameter to Task for assignment score management

--- src/main/java/com/udla/markenx/api/assignments/domain/models/aggregates/Task.java
+++ src/main/java/com/udla/markenx/api/assignments/domain/models/aggregates/Task.java
@@ -2,6 +2,7 @@ package com.udla.markenx.api.assignments.domain.models.aggregates;
 
 import com.udla.markenx.api.assignments.domain.models.valueobjects.AssignmentDeadline;
 import com.udla.markenx.api.assignments.domain.models.valueobjects.AssignmentInfo;
+import com.udla.markenx.api.assignments.domain.models.valueobjects.AssignmentScore;
 import com.udla.markenx.api.assignments.domain.models.valueobjects.AssignmentStatus;
 import com.udla.markenx.api.shared.domain.models.valueobjects.LifecycleStatus;
 import org.jspecify.annotations.NonNull;
@@ -14,9 +15,10 @@ public class Task extends Assignment {
             AssignmentId id,
             AssignmentInfo info,
             AssignmentDeadline deadline,
+            AssignmentScore minScoreToPass,
             AssignmentStatus status,
             String academicTermId) {
-        super(id, info, deadline, status, academicTermId);
+        super(id, info, deadline, minScoreToPass, status, academicTermId);
     }
     public Task(
             String id,
@@ -24,29 +26,42 @@ public class Task extends Assignment {
             long code,
             AssignmentInfo info,
             AssignmentDeadline deadline,
+            AssignmentScore minScoreToPass,
             AssignmentStatus status,
             String academicTermId
     ) {
-        super(id, lifecycleStatus, code, info, deadline, status, academicTermId);
+        super(id, lifecycleStatus, code, info, deadline, minScoreToPass, status, academicTermId);
     }
 
-    public static @NonNull Task create(AssignmentInfo info, LocalDateTime deadline, String academicTermId) {
+    public static @NonNull Task create(
+            AssignmentInfo info,
+            LocalDateTime deadline,
+            AssignmentScore minScoreToPass,
+            String academicTermId
+    ) {
         var id = AssignmentId.generate();
         return new Task(
                 id,
                 info,
                 AssignmentDeadline.future(deadline),
+                minScoreToPass,
                 AssignmentStatus.NOT_STARTED,
                 academicTermId
         );
     }
 
-    public static @NonNull Task createHistorical(AssignmentInfo info, LocalDateTime deadline, String academicTermId) {
+    public static @NonNull Task createHistorical(
+            AssignmentInfo info,
+            LocalDateTime deadline,
+            AssignmentScore minScoreToPass,
+            String academicTermId
+    ) {
         var id = AssignmentId.generate();
         return new Task(
                 id,
                 info,
                 AssignmentDeadline.historical(deadline),
+                minScoreToPass,
                 AssignmentStatus.NOT_STARTED,
                 academicTermId
         );
