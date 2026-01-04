870b131b0e40d1775a903975a1b7857449d7dd2a
Christian Jacome <christian.jacome.mora@udla.edu.ec>
2026-01-04 09:05:53 -0500

    feat: Add getAll method to TaskQueryUseCase for retrieving all tasks

--- src/main/java/com/udla/markenx/api/assignments/application/ports/incoming/TaskQueryUseCase.java
+++ src/main/java/com/udla/markenx/api/assignments/application/ports/incoming/TaskQueryUseCase.java
@@ -4,6 +4,9 @@ import com.udla.markenx.api.assignments.application.queries.GetAllTasksPaginated
 import com.udla.markenx.api.assignments.domain.models.aggregates.Task;
 import org.springframework.data.domain.Page;
 
+import java.util.List;
+
 public interface TaskQueryUseCase {
+    List<Task> getAll();
     Page<Task> getAllPaginated(GetAllTasksPaginatedQuery query);
 }
