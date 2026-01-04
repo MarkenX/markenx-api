2786ffd1efb4d6d1c0f90e633d1d41dbcc875ac9
Christian Jacome <christian.jacome.mora@udla.edu.ec>
2026-01-04 00:27:25 -0500

    feat: Rename InvalidAcademicTermIdException to InvalidCourseIdException for accuracy

--- src/main/java/com/udla/markenx/api/assignments/domain/exceptions/InvalidAcademicTermIdException.java
+++
@@ -1,7 +0,0 @@
-package com.udla.markenx.api.assignments.domain.exceptions;
-
-public class InvalidAcademicTermIdException extends AssignmentException {
-    public InvalidAcademicTermIdException() {
-        super("El identificador del periodo académico no puede estar vacío ni contener espacios en blanco");
-    }
-}
