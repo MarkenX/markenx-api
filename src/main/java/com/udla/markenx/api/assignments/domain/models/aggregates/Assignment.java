c06e87ecf21e599349c11de91ab7ecc3e0016ad7
Christian Jacome <christian.jacome.mora@udla.edu.ec>
2026-01-03 21:17:31 -0500

    feat: Implement Assignment class with properties and constructor

--- src/main/java/com/udla/markenx/api/assignments/domain/models/aggregates/Assignment.java
+++ src/main/java/com/udla/markenx/api/assignments/domain/models/aggregates/Assignment.java
@@ -1,4 +1,37 @@
 package com.udla.markenx.api.assignments.domain.models.aggregates;
 
-public class Assignment {
+import com.udla.markenx.api.assignments.domain.models.valueobjects.AssignmentStatus;
+import com.udla.markenx.api.shared.domain.models.aggregates.Entity;
+
+import java.time.LocalDate;
+
+public class Assignment extends Entity {
+
+    private final AssignmentId id;
+
+    private long code;
+    private String title;
+    private String description;
+    private LocalDate dueDate;
+    private AssignmentStatus status;
+
+    private String academicTermId;
+
+    private Assignment(
+            AssignmentId id,
+            long code,
+            String title,
+            String description,
+            LocalDate dueDate,
+            AssignmentStatus status,
+            String academicTermId) {
+        super();
+        this.id = id;
+        this.code = code;
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.status = status;
+        this.academicTermId = academicTermId;
+    }
 }
