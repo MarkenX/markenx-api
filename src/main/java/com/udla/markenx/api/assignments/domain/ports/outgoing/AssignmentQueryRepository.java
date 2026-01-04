46ded19ef0b8ac6b0df21697c699b8af20c057be
Christian Jacome <christian.jacome.mora@udla.edu.ec>
2026-01-04 09:07:35 -0500

    feat: Add methods for retrieving all assignments in AssignmentQueryRepository

--- src/main/java/com/udla/markenx/api/assignments/domain/ports/outgoing/AssignmentQueryRepository.java
+++ src/main/java/com/udla/markenx/api/assignments/domain/ports/outgoing/AssignmentQueryRepository.java
@@ -1,4 +1,12 @@
 package com.udla.markenx.api.assignments.domain.ports.outgoing;
 
+import com.udla.markenx.api.assignments.domain.models.aggregates.Task;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
 public interface AssignmentQueryRepository {
+    List<Task> findAll();
+    Page<Task> findAllPaginated(Pageable pageable);
 }
