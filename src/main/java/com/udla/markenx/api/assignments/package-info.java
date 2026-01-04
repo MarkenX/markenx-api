63fccf23f2474f792e3bd6abde0cccf44b3e0485
Christian Jacome <christian.jacome.mora@udla.edu.ec>
2026-01-03 21:09:17 -0500

    feat: Enhance Assignment Management module with allowed dependencies

--- src/main/java/com/udla/markenx/api/assignments/package-info.java
+++ src/main/java/com/udla/markenx/api/assignments/package-info.java
@@ -1,4 +1,12 @@
-@ApplicationModule(displayName = "Assignment Management")
+@ApplicationModule(
+        displayName = "Assignment Management",
+        allowedDependencies = {
+                "shared::aggregates",
+                "shared::valueobjects",
+                "shared::exceptions",
+                "shared::dtos",
+        }
+)
 package com.udla.markenx.api.assignments;
 
 import org.springframework.modulith.ApplicationModule;
