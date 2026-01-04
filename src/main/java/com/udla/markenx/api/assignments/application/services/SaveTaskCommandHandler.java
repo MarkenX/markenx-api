95246d9ac10dfffc9043f6886e66bc325949379c
Christian Jacome <christian.jacome.mora@udla.edu.ec>
2026-01-04 09:16:47 -0500

    feat: Enhance SaveTaskCommandHandler to create historical tasks with new AssignmentInfo and AssignmentScore

--- src/main/java/com/udla/markenx/api/assignments/application/services/SaveTaskCommandHandler.java
+++ src/main/java/com/udla/markenx/api/assignments/application/services/SaveTaskCommandHandler.java
@@ -3,10 +3,36 @@ package com.udla.markenx.api.assignments.application.services;
 import com.udla.markenx.api.assignments.application.commands.SaveTaskCommand;
 import com.udla.markenx.api.assignments.application.ports.incoming.SaveTaskUseCase;
 import com.udla.markenx.api.assignments.domain.models.aggregates.Task;
+import com.udla.markenx.api.assignments.domain.models.valueobjects.AssignmentInfo;
+import com.udla.markenx.api.assignments.domain.models.valueobjects.AssignmentScore;
+import org.jspecify.annotations.NonNull;
 
 public class SaveTaskCommandHandler implements SaveTaskUseCase {
     @Override
-    public Task handle(SaveTaskCommand command) {
+    public Task handle(@NonNull SaveTaskCommand command) {
+
+        var info = new AssignmentInfo(command.title(), command.summary());
+        var minScoreToPass = new AssignmentScore(command.minScoreToPass());
+
+        Task newTask;
+        if (command.isHistorical()) {
+            newTask = Task.createHistorical(
+                    info,
+                    command.deadline(),
+                    minScoreToPass,
+                    command.courseId(),
+                    command.maxAttempts()
+            );
+        } else {
+            newTask = Task.create(
+                    info,
+                    command.deadline(),
+                    minScoreToPass,
+                    command.courseId(),
+                    command.maxAttempts()
+            );
+        }
+
         return null;
     }
 }
