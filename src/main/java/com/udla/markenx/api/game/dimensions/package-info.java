1eb055abc6d64dfca06b24871eb60b685d230b02
Christian Jacome <christian.jacome.mora@udla.edu.ec>
2026-01-05 01:57:12 -0500

    feat: Add shared::exceptions to allowedDependencies in package-info

--- src/main/java/com/udla/markenx/api/game/dimensions/package-info.java
+++ src/main/java/com/udla/markenx/api/game/dimensions/package-info.java
@@ -3,6 +3,7 @@
         allowedDependencies = {
                 "shared::aggregates",
                 "shared::valueobjects",
+                "shared::exceptions",
         })
 package com.udla.markenx.api.game.dimensions;
 
