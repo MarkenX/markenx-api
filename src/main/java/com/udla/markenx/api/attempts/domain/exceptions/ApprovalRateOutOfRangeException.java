61aa605d6f25465c68ca5f78601e8f7784af4e05
Christian Jacome <christian.jacome.mora@udla.edu.ec>
2026-01-04 19:48:47 -0500

    feat: Add exception classes for validation in Attempts Management module

---
+++ src/main/java/com/udla/markenx/api/attempts/domain/exceptions/ApprovalRateOutOfRangeException.java
@@ -0,0 +1,8 @@
+package com.udla.markenx.api.attempts.domain.exceptions;
+
+public class ApprovalRateOutOfRangeException extends AttemptException {
+    public ApprovalRateOutOfRangeException() {
+        super("La tasa de aprobación debe estar entre 0.0 y 1.0");
+    }
+}
+
---
+++ src/main/java/com/udla/markenx/api/attempts/domain/exceptions/ProfileScoreOutOfRangeException.java
@@ -0,0 +1,8 @@
+package com.udla.markenx.api.attempts.domain.exceptions;
+
+public class ProfileScoreOutOfRangeException extends AttemptException {
+    public ProfileScoreOutOfRangeException() {
+        super("La puntuación del perfil debe estar entre 0.0 y 1.0");
+    }
+}
+
