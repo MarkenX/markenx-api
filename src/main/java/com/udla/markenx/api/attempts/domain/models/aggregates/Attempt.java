21245d47c9689c7f08f85e02a47f337f30316773
Christian Jacome <christian.jacome.mora@udla.edu.ec>
2026-01-04 19:59:26 -0500

    feat: Add student ID validation to Attempt class with InvalidStudentIdException

--- src/main/java/com/udla/markenx/api/attempts/domain/models/aggregates/Attempt.java
+++ src/main/java/com/udla/markenx/api/attempts/domain/models/aggregates/Attempt.java
@@ -1,5 +1,6 @@
 package com.udla.markenx.api.attempts.domain.models.aggregates;
 
+import com.udla.markenx.api.attempts.domain.exceptions.InvalidStudentIdException;
 import com.udla.markenx.api.attempts.domain.exceptions.InvalidTaskIdException;
 import com.udla.markenx.api.attempts.domain.models.valueobjects.AttemptResult;
 import com.udla.markenx.api.shared.domain.models.aggregates.Entity;
@@ -21,7 +22,7 @@ public class Attempt extends Entity {
         this.id = id;
         this.result = result;
         this.taskId = validateTaskId(taskId);
-        this.studentId = studentId;
+        this.studentId = validateStudentId(studentId);
     }
 
     // region Validations
@@ -31,6 +32,13 @@ public class Attempt extends Entity {
             throw new InvalidTaskIdException();
         }
         return taskId;
+    }
+
+    public String validateStudentId(String studentId) {
+        if (studentId == null || studentId.isBlank()) {
+            throw new InvalidStudentIdException();
+        }
+        return studentId;
     }
 
     // endregion
