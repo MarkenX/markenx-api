6edf3db405959576c6938fe2c85e3234cba6cb59
ChrisJMora2003 <christian.jacome.mora@udla.edu.ec>
2026-01-05 12:09:16 -0500

    feat: Add title and description fields to Scenario class

--- src/main/java/com/udla/markenx/api/game/scenarios/domain/models/aggregates/Scenario.java
+++ src/main/java/com/udla/markenx/api/game/scenarios/domain/models/aggregates/Scenario.java
@@ -3,10 +3,17 @@ package com.udla.markenx.api.game.scenarios.domain.models.aggregates;
 public class Scenario {
 
     private final ScenarioId id;
+    private long code;
+    private String title;
+    private String description;
 
     public Scenario(
-            ScenarioId id
+            ScenarioId id,
+            String title,
+            String description
     ) {
         this.id = id;
+        this.title = title;
+        this.description = description;
     }
 }
