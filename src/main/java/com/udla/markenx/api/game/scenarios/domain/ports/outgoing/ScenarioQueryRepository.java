1210b269e1ffec1aa0a4f49e73ca3bc67417ed0d
ChrisJMora2003 <christian.jacome.mora@udla.edu.ec>
2026-01-05 14:31:53 -0500

    feat: Add findAll methods to ScenarioQueryRepository interface

--- src/main/java/com/udla/markenx/api/game/scenarios/domain/ports/outgoing/ScenarioQueryRepository.java
+++ src/main/java/com/udla/markenx/api/game/scenarios/domain/ports/outgoing/ScenarioQueryRepository.java
@@ -1,4 +1,13 @@
 package com.udla.markenx.api.game.scenarios.domain.ports.outgoing;
 
+import com.udla.markenx.api.game.scenarios.domain.models.aggregates.Scenario;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
 public interface ScenarioQueryRepository {
+    List<Scenario> findAll();
+    Page<@NotNull Scenario> findAll(Pageable pageable);
 }
