007b0c3789eed83e4796d6f5ab78576cca161726
ChrisJMora2003 <christian.jacome.mora@udla.edu.ec>
2026-01-05 09:12:26 -0500

    feat: Add ScenarioId field to Scenario class and initialize via constructor

--- src/main/java/com/udla/markenx/api/game/scenarios/domain/models/aggregates/Scenario.java
+++ src/main/java/com/udla/markenx/api/game/scenarios/domain/models/aggregates/Scenario.java
@@ -1,4 +1,12 @@
 package com.udla.markenx.api.game.scenarios.domain.models.aggregates;
 
 public class Scenario {
+
+    private final ScenarioId id;
+
+    public Scenario(
+            ScenarioId id
+    ) {
+        this.id = id;
+    }
 }
