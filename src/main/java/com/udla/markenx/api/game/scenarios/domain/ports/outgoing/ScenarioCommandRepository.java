1499e2d06ed4a62e7e467bf2d9b8f24a5ff5e993
ChrisJMora2003 <christian.jacome.mora@udla.edu.ec>
2026-01-05 14:30:53 -0500

    feat: Add save and findById methods to ScenarioCommandRepository interface

--- src/main/java/com/udla/markenx/api/game/scenarios/domain/ports/outgoing/ScenarioCommandRepository.java
+++ src/main/java/com/udla/markenx/api/game/scenarios/domain/ports/outgoing/ScenarioCommandRepository.java
@@ -1,4 +1,8 @@
 package com.udla.markenx.api.game.scenarios.domain.ports.outgoing;
 
+import com.udla.markenx.api.game.scenarios.domain.models.aggregates.Scenario;
+
 public interface ScenarioCommandRepository {
+    Scenario save(Scenario scenario);
+    Scenario findById(String id);
 }
