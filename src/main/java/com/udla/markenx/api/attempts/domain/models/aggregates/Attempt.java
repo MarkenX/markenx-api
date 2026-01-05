bfd83b508c77ce6f6ea65ad41999a89407e0edfc
Christian Jacome <christian.jacome.mora@udla.edu.ec>
2026-01-04 20:05:18 -0500

    feat: Add status field to Attempt class for improved attempt management

--- src/main/java/com/udla/markenx/api/attempts/domain/models/aggregates/Attempt.java
+++ src/main/java/com/udla/markenx/api/attempts/domain/models/aggregates/Attempt.java
@@ -3,24 +3,28 @@ package com.udla.markenx.api.attempts.domain.models.aggregates;
 import com.udla.markenx.api.attempts.domain.exceptions.InvalidStudentIdException;
 import com.udla.markenx.api.attempts.domain.exceptions.InvalidTaskIdException;
 import com.udla.markenx.api.attempts.domain.models.valueobjects.AttemptResult;
+import com.udla.markenx.api.attempts.domain.models.valueobjects.AttemptStatus;
 import com.udla.markenx.api.shared.domain.models.aggregates.Entity;
 
 public class Attempt extends Entity {
 
     private final AttemptId id;
     private final AttemptResult result;
+    private AttemptStatus status;
     private final String taskId;
     private final String studentId;
 
     private Attempt(
             AttemptId id,
             AttemptResult result,
+            AttemptStatus status,
             String taskId,
             String studentId
     ) {
         super();
         this.id = id;
         this.result = result;
+        this.status = status;
         this.taskId = validateTaskId(taskId);
         this.studentId = validateStudentId(studentId);
     }
