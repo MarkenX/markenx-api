5bc755e1f94bfeb69d36b4ea15e257394eee91c7
Christian Jacome <christian.jacome.mora@udla.edu.ec>
2026-01-05 01:19:27 -0500

    feat: Enhance package-info for Dimension Management with allowedDependencies

--- src/main/java/com/udla/markenx/api/game/dimensions/package-info.java
+++ src/main/java/com/udla/markenx/api/game/dimensions/package-info.java
@@ -1,4 +1,8 @@
-@ApplicationModule(displayName = "Dimension Management")
+@ApplicationModule(
+        displayName = "Dimension Management",
+        allowedDependencies = {
+                "shared::valueobjects",
+        })
 package com.udla.markenx.api.game.dimensions;
 
 import org.springframework.modulith.ApplicationModule;
