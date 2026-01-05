ab266f9043858bbe87f3b81da32167c9f9799658
ChrisJMora2003 <christian.jacome.mora@udla.edu.ec>
2026-01-05 09:08:59 -0500

    feat: Enhance Scenario Management module with allowed dependencies

--- src/main/java/com/udla/markenx/api/game/scenarios/package-info.java
+++ src/main/java/com/udla/markenx/api/game/scenarios/package-info.java
@@ -1,4 +1,12 @@
-@ApplicationModule(displayName = "Scenario Management")
+@ApplicationModule(
+        displayName = "Scenario Management",
+        allowedDependencies = {
+                "shared::aggregates",
+                "shared::valueobjects",
+                "shared::exceptions",
+                "shared::dtos",
+        }
+)
 package com.udla.markenx.api.game.scenarios;
 
 import org.springframework.modulith.ApplicationModule;
