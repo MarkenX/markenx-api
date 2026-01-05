5ce78ec786790b4bbf6d7fe578d8d71b555477cf
Christian Jacome <christian.jacome.mora@udla.edu.ec>
2026-01-05 01:23:54 -0500

    feat: Add Dimension class to represent game dimensions with attributes

---
+++ src/main/java/com/udla/markenx/api/game/dimensions/domain/models/aggregates/Dimension.java
@@ -0,0 +1,24 @@
+package com.udla.markenx.api.game.dimensions.domain.models.aggregates;
+
+public class Dimension {
+
+    private final DimensionId id;
+    private final String name;
+    private final String description;
+    private final double consumerExpectation;
+    private final double productInitialOffer;
+
+    public Dimension(
+            DimensionId id,
+            String name,
+            String description,
+            double consumerExpectation,
+            double productInitialOffer
+    ) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.consumerExpectation = consumerExpectation;
+        this.productInitialOffer = productInitialOffer;
+    }
+}
