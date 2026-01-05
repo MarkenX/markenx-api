41dd0e480081e17fc140cee1ad26c995d8820f7f
Christian Jacome <christian.jacome.mora@udla.edu.ec>
2026-01-04 21:20:04 -0500

    feat: Add NullAttemptResultException and ResultsAlreadyRegisteredException for attempt validation

---
+++ src/main/java/com/udla/markenx/api/attempts/domain/exceptions/ResultsAlreadyRegisteredException.java
@@ -0,0 +1,7 @@
+package com.udla.markenx.api.attempts.domain.exceptions;
+
+public class ResultsAlreadyRegisteredException extends AttemptException {
+    public ResultsAlreadyRegisteredException() {
+        super("Los resultados ya han sido registrados para este intento");
+    }
+}
