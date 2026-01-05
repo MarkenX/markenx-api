c5312d4d17cbe524b986f6b463f3638884472267
Christian Jacome <christian.jacome.mora@udla.edu.ec>
2026-01-05 01:57:17 -0500

    feat: Implement save and findById methods in JdbcDimensionRepository

--- src/main/java/com/udla/markenx/api/game/dimensions/infrastructure/persistence/jdbc/JdbcDimensionRepository.java
+++ src/main/java/com/udla/markenx/api/game/dimensions/infrastructure/persistence/jdbc/JdbcDimensionRepository.java
@@ -2,20 +2,58 @@ package com.udla.markenx.api.game.dimensions.infrastructure.persistence.jdbc;
 
 import com.udla.markenx.api.game.dimensions.domain.models.aggregates.Dimension;
 import com.udla.markenx.api.game.dimensions.domain.ports.outgoing.DimensionCommandRepository;
+import com.udla.markenx.api.shared.application.exceptions.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.jspecify.annotations.NonNull;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 @Repository
 @RequiredArgsConstructor
 public class JdbcDimensionRepository implements DimensionCommandRepository {
 
+    private final DimensionRowMapper rowMapper = new DimensionRowMapper();
+    private final JdbcTemplate jdbcTemplate;
+
     @Override
-    public Dimension save(Dimension task) {
-        return null;
+    public Dimension save(@NonNull Dimension dimension) {
+        jdbcTemplate.update("""
+        INSERT INTO dimensions
+        (id, name, description, consumer_expectation, product_initial_offer)
+        VALUES (?, ?, ?, ?, ?)
+        """,
+                dimension.getId().value(),  // Asumiendo que DimensionId tiene método value()
+                dimension.getName(),
+                dimension.getDescription(),
+                dimension.getConsumerExpectation(),
+                dimension.getProductInitialOffer()
+        );
+
+        return jdbcTemplate.queryForObject("""
+                SELECT *
+                FROM dimensions
+                WHERE id = ?
+                """,
+                rowMapper,
+                dimension.getId().value());
     }
 
     @Override
     public Dimension findById(String id) {
-        return null;
+        try {
+            return jdbcTemplate.queryForObject("""
+            SELECT *
+            FROM dimensions
+            WHERE id = ?
+            """,
+                    rowMapper,
+                    id
+            );
+        } catch (EmptyResultDataAccessException ex) {
+            throw new EntityNotFoundException(
+                    "No se encontró la dimensión con el identificador: " + id
+            );
+        }
     }
 }
