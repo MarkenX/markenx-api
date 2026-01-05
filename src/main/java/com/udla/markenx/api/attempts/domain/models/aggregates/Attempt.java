9390428dc60309589c510b4ed9c5ecdea27606ca
Christian Jacome <christian.jacome.mora@udla.edu.ec>
2026-01-04 19:49:47 -0500

    feat: Add AttemptResult to Attempt class for managing attempt outcomes

--- src/main/java/com/udla/markenx/api/attempts/domain/models/aggregates/Attempt.java
+++ src/main/java/com/udla/markenx/api/attempts/domain/models/aggregates/Attempt.java
@@ -1,15 +1,19 @@
 package com.udla.markenx.api.attempts.domain.models.aggregates;
 
+import com.udla.markenx.api.attempts.domain.models.valueobjects.AttemptResult;
 import com.udla.markenx.api.shared.domain.models.aggregates.Entity;
 
 public class Attempt extends Entity {
 
     private final AttemptId id;
+    private final AttemptResult result;
 
     private Attempt(
-            AttemptId id
+            AttemptId id,
+            AttemptResult result
     ) {
         super();
         this.id = id;
+        this.result = result;
     }
 }
