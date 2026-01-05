f6a2fd808be759c125b8c97399a29d34ca5d3189
Christian Jacome <christian.jacome.mora@udla.edu.ec>
2026-01-04 19:53:28 -0500

    feat: Add task ID validation to Attempt class with InvalidTaskIdException

--- src/main/java/com/udla/markenx/api/attempts/domain/models/aggregates/Attempt.java
+++ src/main/java/com/udla/markenx/api/attempts/domain/models/aggregates/Attempt.java
@@ -1,5 +1,6 @@
 package com.udla.markenx.api.attempts.domain.models.aggregates;
 
+import com.udla.markenx.api.attempts.domain.exceptions.InvalidTaskIdException;
 import com.udla.markenx.api.attempts.domain.models.valueobjects.AttemptResult;
 import com.udla.markenx.api.shared.domain.models.aggregates.Entity;
 
@@ -7,13 +8,27 @@ public class Attempt extends Entity {
 
     private final AttemptId id;
     private final AttemptResult result;
+    private final String taskId;
 
     private Attempt(
             AttemptId id,
-            AttemptResult result
+            AttemptResult result,
+            String taskId
     ) {
         super();
         this.id = id;
         this.result = result;
+        this.taskId = validateTaskId(taskId);
     }
+
+    // region Validations
+
+    public String validateTaskId(String taskId) {
+        if (taskId == null || taskId.isBlank()) {
+            throw new InvalidTaskIdException();
+        }
+        return taskId;
+    }
+
+    // endregion
 }
