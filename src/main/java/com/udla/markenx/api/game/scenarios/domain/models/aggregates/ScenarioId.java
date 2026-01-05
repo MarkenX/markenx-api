c89ee8c7a7dfa28d63bd73b1f44614071eb150f9
ChrisJMora2003 <christian.jacome.mora@udla.edu.ec>
2026-01-05 09:10:08 -0500

    feat: Extend ScenarioId to inherit from Identifier and add UUID generation

--- src/main/java/com/udla/markenx/api/game/scenarios/domain/models/aggregates/ScenarioId.java
+++ src/main/java/com/udla/markenx/api/game/scenarios/domain/models/aggregates/ScenarioId.java
@@ -1,4 +1,16 @@
 package com.udla.markenx.api.game.scenarios.domain.models.aggregates;
 
-public class ScenarioId {
+import com.udla.markenx.api.shared.domain.models.aggregates.Identifier;
+import org.jetbrains.annotations.Contract;
+import org.jspecify.annotations.NonNull;
+
+public class ScenarioId extends Identifier {
+    public ScenarioId(String value) {
+        super(value);
+    }
+
+    @Contract(" -> new")
+    public static @NonNull ScenarioId generate() {
+        return new ScenarioId(java.util.UUID.randomUUID().toString());
+    }
 }
