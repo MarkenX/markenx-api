f09ecdb72cd4111fd00129e4c9b690be6cef62d5
Christian Jacome <christian.jacome.mora@udla.edu.ec>
2026-01-06 23:03:50 -0500

    feat: Add findById and findByScenarioId methods to DimensionQueryRepository and DimensionQueryUseCase

--- src/main/java/com/udla/markenx/api/game/dimensions/application/ports/incoming/DimensionQueryUseCase.java
+++ src/main/java/com/udla/markenx/api/game/dimensions/application/ports/incoming/DimensionQueryUseCase.java
@@ -3,7 +3,10 @@ package com.udla.markenx.api.game.dimensions.application.ports.incoming;
 import com.udla.markenx.api.game.dimensions.domain.models.aggregates.Dimension;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface DimensionQueryUseCase {
     List<Dimension> getAll();
+    Optional<Dimension> getById(String id);
+    List<Dimension> getByScenarioId(String scenarioId);
 }
--- src/main/java/com/udla/markenx/api/game/dimensions/domain/ports/outgoing/DimensionQueryRepository.java
+++ src/main/java/com/udla/markenx/api/game/dimensions/domain/ports/outgoing/DimensionQueryRepository.java
@@ -3,7 +3,10 @@ package com.udla.markenx.api.game.dimensions.domain.ports.outgoing;
 import com.udla.markenx.api.game.dimensions.domain.models.aggregates.Dimension;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface DimensionQueryRepository {
     List<Dimension> findAll();
+    Optional<Dimension> findById(String id);
+    List<Dimension> findByScenarioId(String scenarioId);
 }
