4ef4ffa83428a738f77e01655f0916052570da07
Christian Jacome <christian.jacome.mora@udla.edu.ec>
2026-01-06 23:14:55 -0500

    Revert "feat: Implement consumer command and query repositories with JDBC and jOOQ persistence"
    This reverts commit 3dfafb110d1c590589965368d5a1513bd79c7190.

--- src/main/java/com/udla/markenx/api/game/consumers/domain/ports/outgoing/ConsumerCommandRepository.java
+++
@@ -1,8 +0,0 @@
-package com.udla.markenx.api.game.consumers.domain.ports.outgoing;
-
-import com.udla.markenx.api.game.consumers.domain.models.aggregates.Consumer;
-
-public interface ConsumerCommandRepository {
-    Consumer save(Consumer consumer);
-    Consumer findById(String id);
-}
