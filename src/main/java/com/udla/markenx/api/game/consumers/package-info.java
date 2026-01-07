ba20b0f948792b9d7d2cc5faa817c97d565d08a2
Christian Jacome <christian.jacome.mora@udla.edu.ec>
2026-01-06 23:04:02 -0500

    feat: Update ApplicationModule annotations for Scenario, Action, Consumer, and Event Management with allowedDependencies

--- src/main/java/com/udla/markenx/api/game/consumers/package-info.java
+++ src/main/java/com/udla/markenx/api/game/consumers/package-info.java
@@ -1,4 +1,11 @@
-@ApplicationModule(displayName = "Consumer Management")
+@ApplicationModule(
+        displayName = "Consumer Management",
+        allowedDependencies = {
+                "shared::aggregates",
+                "shared::valueobjects",
+                "shared::exceptions"
+        }
+)
 package com.udla.markenx.api.game.consumers;
 
 import org.springframework.modulith.ApplicationModule;
--- src/main/java/com/udla/markenx/api/game/scenarios/package-info.java
+++ src/main/java/com/udla/markenx/api/game/scenarios/package-info.java
@@ -1,9 +1,16 @@
-@ApplicationModule(displayName = "Scenario Management", allowedDependencies = {
+@ApplicationModule(
+        displayName = "Scenario Management",
+        allowedDependencies = {
                 "shared::aggregates",
                 "shared::valueobjects",
                 "shared::exceptions",
                 "shared::dtos",
-})
+                "game.dimensions::ports-incoming",
+                "game.consumers::ports-incoming",
+                "game.actions::ports-incoming",
+                "game.events::ports-incoming"
+        }
+)
 package com.udla.markenx.api.game.scenarios;
 
 import org.springframework.modulith.ApplicationModule;
