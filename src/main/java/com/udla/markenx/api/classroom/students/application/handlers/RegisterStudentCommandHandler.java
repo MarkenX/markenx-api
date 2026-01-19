bafda1d767dde75ef64d4ff2fa3376046398130d
ChrisJMora2003 <christian.jacome.mora@udla.edu.ec>
2026-01-19 16:22:22 -0500

    refactor: Rename package for RegisterStudentCommandHandler to improve clarity

--- src/main/java/com/udla/markenx/api/classroom/students/application/handlers/RegisterStudentCommandHandler.java
+++ src/main/java/com/udla/markenx/api/classroom/students/application/handlers/RegisterStudentCommandHandler.java
@@ -1,4 +1,4 @@
-package com.udla.markenx.api.classroom.students.application.services;
+package com.udla.markenx.api.classroom.students.application.handlers;
 
 import com.udla.markenx.api.classroom.students.application.ports.in.commands.RegisterStudentCommand;
 import com.udla.markenx.api.classroom.students.application.ports.in.usecases.RegisterStudentUseCase;
