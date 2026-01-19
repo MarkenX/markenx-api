bf6d87c1424074bd9b69a557c578bf476b8fa92f
ChrisJMora2003 <christian.jacome.mora@udla.edu.ec>
2026-01-19 15:21:11 -0500

    refactor: Update QueryCourseService and QueryCourseUseCase to return CoursePortDTO instead of Course

--- src/main/java/com/udla/markenx/api/classroom/courses/application/ports/in/usecases/QueryCourseUseCase.java
+++ src/main/java/com/udla/markenx/api/classroom/courses/application/ports/in/usecases/QueryCourseUseCase.java
@@ -1,9 +1,10 @@
 package com.udla.markenx.api.classroom.courses.application.ports.in.usecases;
 
+import com.udla.markenx.api.classroom.courses.application.ports.in.dtos.CoursePortDTO;
 import com.udla.markenx.api.classroom.courses.application.ports.in.queries.CoursePageQueryCriteria;
 import com.udla.markenx.api.classroom.courses.domain.models.aggregates.Course;
 import org.springframework.data.domain.Page;
 
 public interface QueryCourseUseCase {
-    Page<Course> listCoursesPage(CoursePageQueryCriteria criteria);
+    Page<CoursePortDTO> listCoursesPage(CoursePageQueryCriteria criteria);
 }
--- src/main/java/com/udla/markenx/api/classroom/courses/application/services/QueryCourseService.java
+++ src/main/java/com/udla/markenx/api/classroom/courses/application/services/QueryCourseService.java
@@ -1,5 +1,7 @@
 package com.udla.markenx.api.classroom.courses.application.services;
 
+import com.udla.markenx.api.classroom.courses.application.ports.in.dtos.CoursePortDTO;
+import com.udla.markenx.api.classroom.courses.application.ports.in.mappers.CoursePortMapper;
 import com.udla.markenx.api.classroom.courses.application.ports.in.usecases.QueryCourseUseCase;
 import com.udla.markenx.api.classroom.courses.application.ports.in.queries.CoursePageQueryCriteria;
 import com.udla.markenx.api.classroom.courses.domain.models.aggregates.Course;
@@ -15,10 +17,11 @@ import org.springframework.stereotype.Service;
 public class QueryCourseService implements QueryCourseUseCase {
 
     private final CourseQueryRepository repository;
+    private final CoursePortMapper mapper = new CoursePortMapper();
 
     @Override
-    public Page<Course> listCoursesPage(@NonNull CoursePageQueryCriteria criteria) {
+    public Page<CoursePortDTO> listCoursesPage(@NonNull CoursePageQueryCriteria criteria) {
         var pageable = PageRequest.of(criteria.page(), criteria.size());
-        return repository.findAllPaginated(pageable);
+        return repository.findAllPaginated(pageable).map(mapper::toDTO);
     }
 }
