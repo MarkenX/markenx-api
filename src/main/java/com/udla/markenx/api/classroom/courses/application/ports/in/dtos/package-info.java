52a28a529ede6ca28edaa12fe7690345e0a52701
ChrisJMora2003 <christian.jacome.mora@udla.edu.ec>
2026-01-19 15:28:41 -0500

    refactor: Add NamedInterface annotation to package-info.java for commands, dtos, queries, and usecases

---
+++ src/main/java/com/udla/markenx/api/classroom/courses/application/ports/in/dtos/package-info.java
@@ -0,0 +1,4 @@
+@NamedInterface("public")
+package com.udla.markenx.api.classroom.courses.application.ports.in.dtos;
+
+import org.springframework.modulith.NamedInterface;
--- src/main/java/com/udla/markenx/api/classroom/courses/application/ports/in/package-info.java
+++
@@ -1,4 +0,0 @@
-@NamedInterface("ports-incoming")
-package com.udla.markenx.api.classroom.courses.application.ports.in;
-
-import org.springframework.modulith.NamedInterface;
