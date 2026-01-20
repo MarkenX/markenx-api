aace023d57bf4a395e7ed7dc2a12d595781b81ce
ChrisJMora2003 <christian.jacome.mora@udla.edu.ec>
2026-01-20 17:40:33 -0500

    refactor: Update `QueryStudentAttemptsUseCase` to use `StudentAttemptPortDTO` and `List`
    - Replaced paginated retrieval with a simpler `List` return type using `StudentAttemptsQuery`.
    - Introduced `StudentAttemptPortDTO` to encapsulate student attempt data fields.

--- src/main/java/com/udla/markenx/api/classroom/students/application/ports/in/usecases/QueryStudentAttemptsUseCase.java
+++ src/main/java/com/udla/markenx/api/classroom/students/application/ports/in/usecases/QueryStudentAttemptsUseCase.java
@@ -1,9 +1,10 @@
 package com.udla.markenx.api.classroom.students.application.ports.in.usecases;
 
-import com.udla.markenx.api.classroom.students.application.ports.in.queries.StudentPageQueryCriteria;
-import com.udla.markenx.api.game.attempts.application.ports.in.dtos.AttemptPortDTO;
-import org.springframework.data.domain.Page;
+import com.udla.markenx.api.classroom.students.application.ports.in.dtos.StudentAttemptPortDTO;
+import com.udla.markenx.api.classroom.students.application.ports.in.queries.StudentAttemptsQuery;
+
+import java.util.List;
 
 public interface QueryStudentAttemptsUseCase {
-    Page<AttemptPortDTO> getAllPaginated(StudentPageQueryCriteria query);
+    List<StudentAttemptPortDTO> getAll(StudentAttemptsQuery query);
 }
