c6e0e88ead38cd955b5c44c564c1af5399960cee
ChrisJMora2003 <christian.jacome.mora@udla.edu.ec>
2026-01-20 16:46:42 -0500

    refactor: Rename `GetAllStudentsPaginatedQuery` to `StudentPageQueryCriteria`

--- src/main/java/com/udla/markenx/api/classroom/students/application/ports/in/queries/StudentPageQueryCriteria.java
+++ src/main/java/com/udla/markenx/api/classroom/students/application/ports/in/queries/StudentPageQueryCriteria.java
@@ -1,4 +1,4 @@
 package com.udla.markenx.api.classroom.students.application.ports.in.queries;
 
-public record GetAllStudentsPaginatedQuery(int page, int size) {
+public record StudentPageQueryCriteria(int page, int size) {
 }
