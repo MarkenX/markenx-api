41d361bde88564af9fd14744ecde2e905748f394
ChrisJMora2003 <christian.jacome.mora@udla.edu.ec>
2026-01-21 12:41:20 -0500

    refactor: Add `toStudentPortDTO` method to `StudentPortMapper` and rename `toDTO` to `toStudentAttemptPortDTO`

--- src/main/java/com/udla/markenx/api/classroom/students/application/ports/in/mappers/StudentPortMapper.java
+++ src/main/java/com/udla/markenx/api/classroom/students/application/ports/in/mappers/StudentPortMapper.java
@@ -1,12 +1,23 @@
 package com.udla.markenx.api.classroom.students.application.ports.in.mappers;
 
 import com.udla.markenx.api.classroom.students.application.ports.in.dtos.StudentAttemptPortDTO;
+import com.udla.markenx.api.classroom.students.application.ports.in.dtos.StudentPortDTO;
+import com.udla.markenx.api.classroom.students.domain.models.aggregates.Student;
 import com.udla.markenx.api.game.attempts.application.ports.in.dtos.AttemptPortDTO;
 import org.jspecify.annotations.NonNull;
 
 public class StudentPortMapper {
 
-    public StudentAttemptPortDTO toDTO(@NonNull AttemptPortDTO port) {
+    public StudentPortDTO toStudentPortDTO(@NonNull Student domain) {
+        return new StudentPortDTO(
+                domain.getId(),
+                domain.toString(),
+                domain.getFullName(),
+                domain.getCourseId()
+        );
+    }
+
+    public StudentAttemptPortDTO toStudentAttemptPortDTO(@NonNull AttemptPortDTO port) {
         return new StudentAttemptPortDTO(
                 port.attemptId(),
                 port.taskId(),
