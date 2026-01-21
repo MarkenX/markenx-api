4dae802e6bb22d03cf586752eb46a93d37ea9eed
ChrisJMora2003 <christian.jacome.mora@udla.edu.ec>
2026-01-21 11:41:13 -0500

    refactor: Remove `formatCode` method and unused `@Contract` import, simplify `toString` in `Course` entity

--- src/main/java/com/udla/markenx/api/classroom/courses/domain/models/aggregates/Course.java
+++ src/main/java/com/udla/markenx/api/classroom/courses/domain/models/aggregates/Course.java
@@ -5,7 +5,6 @@ import com.udla.markenx.api.classroom.courses.domain.exceptions.InvalidCourseCod
 import com.udla.markenx.api.classroom.courses.domain.exceptions.InvalidCourseNameException;
 import com.udla.markenx.api.shared.domain.models.aggregates.Entity;
 import lombok.Getter;
-import org.jetbrains.annotations.Contract;
 import org.jspecify.annotations.NonNull;
 
 @Getter
@@ -151,18 +150,8 @@ public class Course extends Entity {
         return id.hashCode();
     }
 
-    /**
-     * Formats the course code as a zero-padded four-digit string.
-     *
-     * @return the formatted course code as a string
-     */
-    @Contract(pure = true)
-    private @NonNull String formatCode() {
-        return String.format("%04d", code);
-    }
-
     @Override
     public String toString() {
-        return String.format("%s-%s", name, formatCode());
+        return name;
     }
 }
