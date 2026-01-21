caa40b8aa3d143485d5eb93fb713deb18c1871d5
Christian Jacome <christian.jacome.mora@udla.edu.ec>
2026-01-21 01:55:30 -0500

    refactor: Remove unused IdentityDisableFailedEvent handling from UserIdentityEventListener

--- src/main/java/com/udla/markenx/api/classroom/students/infrastructure/listeners/UserIdentityEventListener.java
+++ src/main/java/com/udla/markenx/api/classroom/students/infrastructure/listeners/UserIdentityEventListener.java
@@ -1,7 +1,6 @@
 package com.udla.markenx.api.classroom.students.infrastructure.listeners;
 
 import com.udla.markenx.api.classroom.students.application.ports.in.usecases.UpdateStudentUseCase;
-import com.udla.markenx.api.shared.domain.events.integration.IdentityDisableFailedEvent;
 import com.udla.markenx.api.shared.domain.events.integration.IdentityDisabledEvent;
 import com.udla.markenx.api.shared.domain.events.integration.IdentityProvisionedEvent;
 import com.udla.markenx.api.shared.domain.events.integration.IdentityProvisioningFailedEvent;
@@ -9,10 +8,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
-/**
- * Listens to integration events from the security module
- * to update student status based on identity provisioning results.
- */
 @Component
 @RequiredArgsConstructor
 public class UserIdentityEventListener {
@@ -32,10 +27,5 @@ public class UserIdentityEventListener {
     @EventListener
     public void on(IdentityDisabledEvent event) {
         useCase.onUserDisabled(event.sourceEntityId());
-    }
-
-    @EventListener
-    public void on(IdentityDisableFailedEvent event) {
-        useCase.onUserDisableFailed(event.sourceEntityId());
     }
 }
