de9e6e8a50a99f9d3814d4a42c4f031cc93bfd09
ChrisJMora2003 <christian.jacome.mora@udla.edu.ec>
2026-01-21 09:58:48 -0500

    refactor: Remove unused `Optional`

--- src/main/java/com/udla/markenx/api/classroom/students/application/ports/in/usecases/QueryStudentsUseCase.java
+++ src/main/java/com/udla/markenx/api/classroom/students/application/ports/in/usecases/QueryStudentsUseCase.java
@@ -4,8 +4,6 @@ import com.udla.markenx.api.classroom.students.application.ports.in.queries.Stud
 import com.udla.markenx.api.classroom.students.query.models.StudentSummaryReadModel;
 import org.springframework.data.domain.Page;
 
-import java.util.Optional;
-
 public interface QueryStudentsUseCase {
     Page<StudentSummaryReadModel> getAllPaginated(StudentPageQueryCriteria query);
     StudentSummaryReadModel findByEmail(String email);
