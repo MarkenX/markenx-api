2a4145685ab8f11f08fbe2bbee4b627dfe013df2
ChrisJMora2003 <christian.jacome.mora@udla.edu.ec>
2026-01-21 09:53:13 -0500

    refactor: Remove unused `Course` import from `CreateCourseUseCase`

--- src/main/java/com/udla/markenx/api/classroom/courses/application/ports/in/usecases/CreateCourseUseCase.java
+++ src/main/java/com/udla/markenx/api/classroom/courses/application/ports/in/usecases/CreateCourseUseCase.java
@@ -2,7 +2,6 @@ package com.udla.markenx.api.classroom.courses.application.ports.in.usecases;
 
 import com.udla.markenx.api.classroom.courses.application.ports.in.commands.CreateCourseCommand;
 import com.udla.markenx.api.classroom.courses.application.ports.in.dtos.CoursePortDTO;
-import com.udla.markenx.api.classroom.courses.domain.models.aggregates.Course;
 
 public interface CreateCourseUseCase {
     CoursePortDTO handle(CreateCourseCommand command);
