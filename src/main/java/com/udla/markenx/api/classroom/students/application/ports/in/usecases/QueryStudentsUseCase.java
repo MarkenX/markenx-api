d8e264d759d363aaa47f344cb2dd70c7d2480800
ChrisJMora2003 <christian.jacome.mora@udla.edu.ec>
2026-01-21 12:33:17 -0500

    refactor: Add `getStudentById` and `listStudents` methods to `QueryStudentsUseCase` interface

--- src/main/java/com/udla/markenx/api/classroom/students/application/ports/in/usecases/QueryStudentsUseCase.java
+++ src/main/java/com/udla/markenx/api/classroom/students/application/ports/in/usecases/QueryStudentsUseCase.java
@@ -1,5 +1,10 @@
 package com.udla.markenx.api.classroom.students.application.ports.in.usecases;
 
-public interface QueryStudentsUseCase {
+import com.udla.markenx.api.classroom.students.application.ports.in.dtos.StudentPortDTO;
 
+import java.util.List;
+
+public interface QueryStudentsUseCase {
+    StudentPortDTO getStudentById(String studentId);
+    List<StudentPortDTO> listStudents();
 }
