5cd9d560c8c516ae284c7fb7862153e0d81f529e
ChrisJMora2003 <christian.jacome.mora@udla.edu.ec>
2026-01-21 12:41:04 -0500

    feat: Implement `QueryStudentsService` with `listStudents` and placeholder for `getStudentById`

---
+++ src/main/java/com/udla/markenx/api/classroom/students/application/services/QueryStudentsService.java
@@ -0,0 +1,31 @@
+package com.udla.markenx.api.classroom.students.application.services;
+
+import com.udla.markenx.api.classroom.students.application.ports.in.dtos.StudentPortDTO;
+import com.udla.markenx.api.classroom.students.application.ports.in.mappers.StudentPortMapper;
+import com.udla.markenx.api.classroom.students.application.ports.in.usecases.QueryStudentsUseCase;
+import com.udla.markenx.api.classroom.students.application.ports.out.StudentQueryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class QueryStudentsService implements QueryStudentsUseCase {
+
+    private final StudentQueryRepository repository;
+
+    private final StudentPortMapper mapper = new StudentPortMapper();
+
+    @Override
+    public StudentPortDTO getStudentById(String studentId) {
+        return null;
+    }
+
+    @Override
+    public List<StudentPortDTO> listStudents() {
+        return repository.findAll().stream()
+                .map(mapper::toStudentPortDTO)
+                .toList();
+    }
+}
