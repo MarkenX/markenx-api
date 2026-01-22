9d193db1ba8cdf1b4fa5d7561b34404e84a37841
Christian Jacome <christian.jacome.mora@udla.edu.ec>
2026-01-21 20:57:41 -0500

    refactor: Add @NonNull annotation to EntityNotFoundException handler parameter in EntityControllerAdvice

--- src/main/java/com/udla/markenx/api/shared/infrastructure/web/rest/EntityControllerAdvice.java
+++ src/main/java/com/udla/markenx/api/shared/infrastructure/web/rest/EntityControllerAdvice.java
@@ -2,6 +2,7 @@ package com.udla.markenx.api.shared.infrastructure.web.rest;
 
 import com.udla.markenx.api.shared.infrastructure.web.dtos.ErrorResponse;
 import com.udla.markenx.api.shared.application.exceptions.EntityNotFoundException;
+import org.jspecify.annotations.NonNull;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -16,7 +17,7 @@ public class EntityControllerAdvice {
     @ExceptionHandler(EntityNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ResponseBody
-    public ErrorResponse handleDomainException(EntityNotFoundException ex) {
+    public ErrorResponse handleDomainException(@NonNull EntityNotFoundException ex) {
         return new ErrorResponse(ENTITY_ERROR_CODE, ex.getMessage());
     }
 }
